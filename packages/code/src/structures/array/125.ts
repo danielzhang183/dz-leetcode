e9b9728c3ef305eac16d2d210ea30064b2f7df92
danielzhang183 <danielzhang182@gmail.com>
2023-02-20 15:20:21 +0800

    question: 125

--- packages/code/src/structures/array/125.ts
+++ packages/code/src/structures/array/125.ts
@@ -1,3 +1,14 @@
 export function isPalindrome(s: string): boolean {
+  s = s.replace(/[_\W]/g, '').toLowerCase()
+  if (!s.length)
+    return true
 
+  let i = 0
+  let j = s.length - 1
+  while (i < j && (s[i] === s[j])) {
+    i++
+    j--
+  }
+
+  return i >= j
 }
--- packages/code/test/structures/array/125.test.ts
+++ packages/code/test/structures/array/125.test.ts
@@ -3,6 +3,11 @@ import { isPalindrome } from '../../../src/structures/array/125'
 
 describe('isPalindrome', () => {
   it('exported', () => {
-    expect(isPalindrome('A man, a plan, a canal: Panama')).toBe('race a car')
+    expect(isPalindrome('A man, a plan, a canal: Panama')).toBe(true)
+    expect(isPalindrome('race a car')).toBe(false)
+    expect(isPalindrome('a')).toBe(true)
+    expect(isPalindrome('aa')).toBe(true)
+    expect(isPalindrome(' ')).toBe(true)
+    expect(isPalindrome('ab_a')).toBe(true)
   })
 })
