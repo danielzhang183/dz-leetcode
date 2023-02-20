2be507d22e7a61f32417eb6fede2c4321b2b545c
danielzhang183 <danielzhang182@gmail.com>
2023-02-20 14:09:41 +0800

    question: 027

--- packages/code/src/structures/array/027.ts
+++ packages/code/src/structures/array/027.ts
@@ -1,3 +1,9 @@
 export function removeElement(nums: number[], val: number): number {
+  let j = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val)
+      nums[j++] = nums[i]
+  }
 
+  return j
 }
--- packages/code/test/structures/array/027.test.ts
+++ packages/code/test/structures/array/027.test.ts
@@ -3,7 +3,8 @@ import { removeElement } from '../../../src/structures/array/027'
 
 describe('removeElement', () => {
   it('exported', () => {
-    expect(removeElement([3, 2, 2, 3])).toBe(3)
-    expect(removeElement([0, 1, 2, 2, 3, 0, 4, 2])).toBe(2)
+    expect(removeElement([3, 2, 2, 3], 3)).toBe(2)
+    expect(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)).toBe(5)
+    expect(removeElement([1], 1)).toBe(0)
   })
 })
