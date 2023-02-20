58f6c5f1c196961d54b3aefe853c06455f48f1dc
danielzhang183 <danielzhang182@gmail.com>
2023-02-20 16:55:14 +0800

    question: 209

--- packages/code/src/structures/array/209.ts
+++ packages/code/src/structures/array/209.ts
@@ -1,3 +1,19 @@
 export function minSubArrayLen(target: number, nums: number[]): number {
+  let sum = 0
+  let start = 0
+  let end = 0
+  let ans = Number.MAX_SAFE_INTEGER
 
+  while (end < nums.length) {
+    sum += nums[end]
+
+    while (sum >= target) {
+      ans = Math.min(ans, end - start + 1)
+      sum -= nums[start++]
+    }
+
+    end++
+  }
+
+  return ans === Number.MAX_SAFE_INTEGER ? 0 : ans
 }
--- packages/code/test/structures/array/209.test.ts
+++ packages/code/test/structures/array/209.test.ts
@@ -3,8 +3,9 @@ import { minSubArrayLen } from '../../../src/structures/array/209'
 
 describe('minSubArrayLen', () => {
   it('exported', () => {
-    expect(minSubArrayLen(7)).toBe([2, 3, 1, 2, 4, 3])
-    expect(minSubArrayLen(4)).toBe([1, 4, 4])
-    expect(minSubArrayLen(11)).toBe([1, 1, 1, 1, 1, 1, 1, 1])
+    expect(minSubArrayLen(7, [2, 3, 1, 2, 4, 3])).toBe(2)
+    expect(minSubArrayLen(4, [1, 4, 4])).toBe(1)
+    expect(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1])).toBe(0)
+    expect(minSubArrayLen(213, [12, 28, 83, 4, 25, 26, 25, 2, 25, 25, 25, 12])).toBe(8)
   })
 })
