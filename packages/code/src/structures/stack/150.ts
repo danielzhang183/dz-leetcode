9deb736856c1eb86cbc1c9268076371d4bf8e1f8
danielzhang183 <danielzhang182@gmail.com>
2023-02-21 13:16:17 +0800

    question: stack 150 medium

--- packages/code/src/structures/stack/150.ts
+++ packages/code/src/structures/stack/150.ts
@@ -1,3 +1,22 @@
 export function evalRPN(tokens: string[]): number {
+  const map: Map<string, (a: number, b: number) => number> = new Map([
+    ['+', (a, b) => a + b],
+    ['-', (a, b) => a - b],
+    ['*', (a, b) => a * b],
+    ['/', (a, b) => a / b > 0 ? Math.floor(a / b) : Math.ceil(a / b)],
+  ])
+  const stack: number[] = []
 
+  for (const token of tokens) {
+    if (map.has(token)) {
+      const b = Number(stack.pop())
+      const a = Number(stack.pop())
+      stack.push(map.get(token)!(a, b))
+    }
+    else {
+      stack.push(Number(token))
+    }
+  }
+
+  return stack[0]
 }
--- packages/code/test/structures/stack/150.test.ts
+++ packages/code/test/structures/stack/150.test.ts
@@ -3,6 +3,8 @@ import { evalRPN } from '../../../src/structures/stack/150'
 
 describe('evalRPN', () => {
   it('exported', () => {
-    expect(evalRPN(['2', '1', '+', '3', '*'])).toBe(['4', '13', '5', '/', '+'])
+    expect(evalRPN(['2', '1', '+', '3', '*'])).toBe(9)
+    expect(evalRPN(['4', '13', '5', '/', '+'])).toBe(6)
+    expect(evalRPN(['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+'])).toBe(22)
   })
 })
