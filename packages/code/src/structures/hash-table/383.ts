4c6cf325155aa82adfe0fc9c7238c19461ec6c2e
danielzhang183 <danielzhang182@gmail.com>
2023-02-21 15:34:25 +0800

    question: hashtable 383 easy

--- packages/code/src/structures/hash-table/383.ts
+++ packages/code/src/structures/hash-table/383.ts
@@ -1,3 +1,13 @@
 export function canConstruct(ransomNote: string, magazine: string): boolean {
+  const table = new Array(26).fill(0)
 
-};
+  for (let i = 0; i < magazine.length; i++)
+    table[magazine.codePointAt(i)! - 'a'.codePointAt(0)!]++
+
+  for (let i = 0; i < ransomNote.length; i++) {
+    if (--table[ransomNote.codePointAt(i)! - 'a'.codePointAt(0)!] < 0)
+      return false
+  }
+
+  return true
+}
--- packages/code/test/structures/hash-table/383.test.ts
+++ packages/code/test/structures/hash-table/383.test.ts
@@ -3,8 +3,8 @@ import { canConstruct } from '../../../src/structures/hash-table/383'
 
 describe('canConstruct', () => {
   it('exported', () => {
-    expect(canConstruct("a")).toBe("b")
-    expect(canConstruct("aa")).toBe("ab")
-    expect(canConstruct("aa")).toBe("aab")
+    expect(canConstruct('a', 'b')).toBe(false)
+    expect(canConstruct('aa', 'ab')).toBe(false)
+    expect(canConstruct('aa', 'aab')).toBe(true)
   })
 })
