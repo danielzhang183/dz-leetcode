90a75a8dfb15ce85c46b2a2c199a56fd7b168805
danielzhang183 <danielzhang182@gmail.com>
2023-02-21 10:46:21 +0800

    question: stack 071 medium

--- packages/code/src/structures/stack/071.ts
+++ packages/code/src/structures/stack/071.ts
@@ -1,3 +1,14 @@
 export function simplifyPath(path: string): string {
+  const stack: string[] = []
 
+  for (const p of path.split('/')) {
+    if (['', '.'].includes(p))
+      continue
+    else if (p === '..')
+      stack.pop()
+    else
+      stack.push(p)
+  }
+
+  return `/${stack.join('/')}`
 }
--- packages/code/test/structures/stack/071.test.ts
+++ packages/code/test/structures/stack/071.test.ts
@@ -3,6 +3,9 @@ import { simplifyPath } from '../../../src/structures/stack/071'
 
 describe('simplifyPath', () => {
   it('exported', () => {
-    expect(simplifyPath('/home/')).toBe('/../')
+    expect(simplifyPath('/home/')).toBe('/home')
+    expect(simplifyPath('/../')).toBe('/')
+    expect(simplifyPath('/home//foo/')).toBe('/home/foo')
+    expect(simplifyPath('/a/./b/../../c/')).toBe('/c')
   })
 })
