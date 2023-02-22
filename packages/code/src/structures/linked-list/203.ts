5f3d2a9c17827297f1206f08d96af86d27a3c843
danielzhang183 <danielzhang182@gmail.com>
2023-02-22 16:14:35 +0800

    question: linklist 203 easy

--- packages/code/src/structures/linked-list/203.ts
+++ packages/code/src/structures/linked-list/203.ts
@@ -1,15 +1,25 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+import type { ListNode } from '../../utils/types'
 
+// recursive
 export function removeElements(head: ListNode | null, val: number): ListNode | null {
-
+  if (head == null)
+    return head
+  head.next = removeElements(head.next, val)
+  return head.val === val ? head.next : head
 }
+
+// traverse
+// export function removeElements(head: ListNode | null, val: number): ListNode | null {
+//   const prehead = new ListNode(-1)
+//   prehead.next = head
+
+//   let curr = prehead
+//   while (curr.next != null) {
+//     if (curr.next.val === val)
+//       curr.next = curr.next.next
+//     else
+//       curr = curr.next
+//   }
+
+//   return prehead.next
+// }
--- packages/code/test/structures/linked-list/203.test.ts
+++ packages/code/test/structures/linked-list/203.test.ts
@@ -1,10 +1,14 @@
 import { describe, expect, it } from 'vitest'
 import { removeElements } from '../../../src/structures/linked-list/203'
+import { createLinkedList, toLinkedListString } from '../../../src/utils'
 
 describe('removeElements', () => {
   it('exported', () => {
-    expect(removeElements([1, 2, 6, 3, 4, 5, 6])).toBe(6)
-    expect(removeElements([])).toBe(1)
-    expect(removeElements([7, 7, 7, 7])).toBe(7)
+    const ll1 = createLinkedList([1, 2, 6, 3, 4, 5, 6])
+    expect(toLinkedListString(removeElements(ll1, 6))).toStrictEqual([1, 2, 3, 4, 5])
+    const ll2 = createLinkedList([])
+    expect(toLinkedListString(removeElements(ll2, 1))).toStrictEqual([])
+    const ll3 = createLinkedList([7, 7, 7, 7])
+    expect(toLinkedListString(removeElements(ll3, 7))).toStrictEqual([])
   })
 })
