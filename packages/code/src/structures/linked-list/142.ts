d44f7fbf05a2e2f5bdc0c1c65212fcba693ded27
danielzhang183 <danielzhang182@gmail.com>
2023-02-23 22:01:14 +0800

    question: linklist 142 medium

--- packages/code/src/structures/linked-list/142.ts
+++ packages/code/src/structures/linked-list/142.ts
@@ -1,15 +1,38 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+import type { ListNode } from '../../utils/types'
 
+// fast & slow pointer
 export function detectCycle(head: ListNode | null): ListNode | null {
+  let fast = head
+  let slow = head
+  while (true) {
+    if (fast == null || fast.next == null)
+      return null
+    fast = fast.next.next
+    slow = slow!.next
+    if (fast === slow)
+      break
+  }
 
+  fast = head
+  while (fast !== slow) {
+    fast = fast!.next
+    slow = slow!.next
+  }
+  return fast
+}
+
+// hasSet
+export function detectCycle1(head: ListNode | null): ListNode | null {
+  const set = new Set()
+
+  let curr: ListNode | null = head
+  while (curr != null) {
+    if (set.has(curr))
+      return curr
+
+    set.add(curr)
+    curr = curr.next
+  }
+
+  return null
 }
--- packages/code/test/structures/linked-list/142.test.ts
+++ packages/code/test/structures/linked-list/142.test.ts
@@ -1,10 +1,17 @@
 import { describe, expect, it } from 'vitest'
 import { detectCycle } from '../../../src/structures/linked-list/142'
+import { createCycleLinkedList } from '../../../src/utils'
 
 describe('detectCycle', () => {
-  it('exported', () => {
-    expect(detectCycle([3, 2, 0, -4])).toBe(1)
-    expect(detectCycle([1, 2])).toBe(0)
-    expect(detectCycle([1])).toBe(-1)
+  it('has cycle', () => {
+    const ll1 = createCycleLinkedList([3, 2, 0, -4], 1)
+    expect(detectCycle(ll1)!.val).toBe(2)
+    const ll2 = createCycleLinkedList([1, 2], 0)
+    expect(detectCycle(ll2)!.val).toStrictEqual(1)
+  })
+
+  it('non cycle', () => {
+    const ll3 = createCycleLinkedList([1])
+    expect(detectCycle(ll3)).toBe(null)
   })
 })
