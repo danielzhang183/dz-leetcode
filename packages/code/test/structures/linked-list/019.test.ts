4d7d4306d9b0b1f5a12df9d9c59b5b712055b4e4
danielzhang183 <danielzhang182@gmail.com>
2023-02-23 14:50:15 +0800

    question: linklist 019 medium

--- packages/code/test/structures/linked-list/019.test.ts
+++ packages/code/test/structures/linked-list/019.test.ts
@@ -1,10 +1,17 @@
 import { describe, expect, it } from 'vitest'
 import { removeNthFromEnd } from '../../../src/structures/linked-list/019'
+import { createLinkedList, toLinkedListString } from '../../../src/utils'
+import type { ListNode } from '../../../src/utils/types'
 
 describe('removeNthFromEnd', () => {
+  let ll: ListNode | null
+
   it('exported', () => {
-    expect(removeNthFromEnd([1, 2, 3, 4, 5])).toBe(2)
-    expect(removeNthFromEnd([1])).toBe(1)
-    expect(removeNthFromEnd([1, 2])).toBe(1)
+    ll = createLinkedList([1, 2, 3, 4, 5])
+    expect(toLinkedListString(removeNthFromEnd(ll, 2))).toStrictEqual([1, 2, 3, 5])
+    ll = createLinkedList([1])
+    expect(toLinkedListString(removeNthFromEnd(ll, 1))).toStrictEqual([])
+    ll = createLinkedList([1, 2])
+    expect(toLinkedListString(removeNthFromEnd(ll, 1))).toStrictEqual([1])
   })
 })
