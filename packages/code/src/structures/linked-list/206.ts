92579c5e330942cbb5b30035bf95e06ffca6baf5
danielzhang183 <danielzhang182@gmail.com>
2023-02-23 13:18:06 +0800

    question: linklist 206 easy

--- packages/code/src/structures/linked-list/206.ts
+++ packages/code/src/structures/linked-list/206.ts
@@ -1,15 +1,33 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+import type { ListNode } from '../../utils/types'
 
+// recursive
 export function reverseList(head: ListNode | null): ListNode | null {
+  // console.log({ head })
+  if (head == null || head.next == null)
+    return head
 
+  const dumpHead = reverseList(head.next)
+  // console.log({ before: dumpHead, next2: head.next.next, next1: head.next })
+  head.next.next = head
+  head.next = null
+  // console.log({ after: toLinkedListString(dumpHead) })
+  return dumpHead
 }
+
+// traverse
+// export function reverseList(head: ListNode | null): ListNode | null {
+//   if (head == null || head.next == null)
+//     return head
+
+//   let prev = null
+//   let temp: ListNode | null = null
+//   let curr = head
+//   while (curr) {
+//     temp = curr.next
+//     curr.next = prev
+//     prev = curr
+//     curr = temp
+//   }
+
+//   return prev
+// }
--- packages/code/test/structures/linked-list/206.test.ts
+++ packages/code/test/structures/linked-list/206.test.ts
@@ -1,8 +1,14 @@
 import { describe, expect, it } from 'vitest'
 import { reverseList } from '../../../src/structures/linked-list/206'
+import { createLinkedList, toLinkedListString } from '../../../src/utils'
 
 describe('reverseList', () => {
   it('exported', () => {
-    expect(reverseList([1, 2, 3, 4, 5])).toBe([1, 2])
+    const ll1 = createLinkedList([1, 2, 3, 4, 5])
+    expect(toLinkedListString(reverseList(ll1))).toStrictEqual([5, 4, 3, 2, 1])
+    // const ll2 = createLinkedList([1, 2])
+    // expect(toLinkedListString(reverseList(ll2))).toStrictEqual([2, 1])
+    // const ll3 = createLinkedList([])
+    // expect(toLinkedListString(reverseList(ll3))).toStrictEqual([])
   })
 })
