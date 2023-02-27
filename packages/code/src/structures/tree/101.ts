c8d0e59cb36dc6ba1d4d84b8d8359547b4146dad
danielzhang183 <danielzhang182@gmail.com>
2023-02-27 10:41:16 +0800

    question: tree 101 easy

--- packages/code/src/structures/tree/101.ts
+++ packages/code/src/structures/tree/101.ts
@@ -1,17 +1,71 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import type { TreeNode } from '../../utils'
+import { Queue } from '../../utils/queue'
 
+// Recursive
+export function isSymmetric1(root: TreeNode | null): boolean {
+  if (root == null)
+    return true
+
+  const helper = (left: TreeNode | null, right: TreeNode | null): boolean => {
+    if (!left && !right)
+      return true
+    if (!left || !right || left.val !== right.val)
+      return false
+
+    return helper(left.left, right.right)
+     && helper(left.right, right.left)
+  }
+
+  return helper(root.left, root.right)
+}
+
+// BFS - Stack
+export function isSymmetric2(root: TreeNode | null): boolean {
+  if (!root)
+    return true
+
+  const stack: (TreeNode | null)[] = [root.left, root.right]
+  while (stack.length) {
+    const right = stack.pop()
+    const left = stack.pop()
+
+    if (!left && !right)
+      continue
+    if (!left || !right || left.val !== right.val)
+      return false
+
+    stack.push(right.right)
+    stack.push(left.left)
+    stack.push(left.right)
+    stack.push(right.left)
+  }
+
+  return true
+}
+
+// BFS - Queue
 export function isSymmetric(root: TreeNode | null): boolean {
+  if (!root)
+    return true
 
+  const q = new Queue<TreeNode | null>()
+  q.enqueue(root.left)
+  q.enqueue(root.right)
+
+  while (!q.isEmpty()) {
+    const left = q.dequeue()
+    const right = q.dequeue()
+
+    if (!left && !right)
+      continue
+    if (!left || !right || left.val !== right.val)
+      return false
+
+    q.enqueue(left.left)
+    q.enqueue(right.right)
+    q.enqueue(right.left)
+    q.enqueue(left.right)
+  }
+
+  return true
 }
--- packages/code/test/structures/tree/101.test.ts
+++ packages/code/test/structures/tree/101.test.ts
@@ -1,8 +1,10 @@
 import { describe, expect, it } from 'vitest'
 import { isSymmetric } from '../../../src/structures/tree/101'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('isSymmetric', () => {
   it('exported', () => {
-    expect(isSymmetric([1, 2, 2, 3, 4, 4, 3])).toBe([1, 2, 2, null, 3, null, 3])
+    expect(isSymmetric(createBinaryTree([1, 2, 2, 3, 4, 4, 3]))).toBe(true)
+    expect(isSymmetric(createBinaryTree([1, 2, 2, null, 3, null, 3]))).toBe(false)
   })
 })
