ea387e44e2cb7658061944cfc9a86215e30e9010
danielzhang183 <danielzhang182@gmail.com>
2023-02-27 11:28:43 +0800

    question: tree 104 easy

--- packages/code/src/structures/tree/104.ts
+++ packages/code/src/structures/tree/104.ts
@@ -1,17 +1,67 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import type { TreeNode } from '../../utils'
+import { Queue } from '../../utils/queue'
 
+// Recursive
+export function maxDepth1(root: TreeNode | null): number {
+  let max = 0
+  const dfs = (root: TreeNode | null, depth = 0) => {
+    if (!root)
+      return 0
+
+    depth++
+    max = Math.max(max, depth)
+    if (root.left)
+      dfs(root.left, depth)
+    if (root.right)
+      dfs(root.right, depth)
+  }
+
+  dfs(root)
+  return max
+}
+
+// BFS - Stack
+export function maxDepth2(root: TreeNode | null): number {
+  if (!root)
+    return 0
+
+  let depth = 0
+  const stack = [root]
+  while (stack.length) {
+    let size = stack.length
+    while (size--) {
+      const curr = stack.shift()!
+      if (curr.left)
+        stack.push(curr.left)
+      if (curr.right)
+        stack.push(curr.right)
+    }
+    depth++
+  }
+
+  return depth
+}
+
+// BFS - Queue
 export function maxDepth(root: TreeNode | null): number {
+  if (!root)
+    return 0
 
+  let depth = 0
+  const q = new Queue<TreeNode | null>()
+  q.enqueue(root)
+
+  while (!q.isEmpty()) {
+    let size = q.size
+    while (size--) {
+      const curr = q.dequeue()!
+      if (curr.left)
+        q.enqueue(curr.left)
+      if (curr.right)
+        q.enqueue(curr.right)
+    }
+    depth++
+  }
+
+  return depth
 }
--- packages/code/test/structures/tree/104.test.ts
+++ packages/code/test/structures/tree/104.test.ts
@@ -1,8 +1,10 @@
 import { describe, expect, it } from 'vitest'
 import { maxDepth } from '../../../src/structures/tree/104'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('maxDepth', () => {
   it('exported', () => {
-    expect(maxDepth([3, 9, 20, null, null, 15, 7])).toBe([1, null, 2])
+    expect(maxDepth(createBinaryTree([3, 9, 20, null, null, 15, 7]))).toBe(3)
+    expect(maxDepth(createBinaryTree([1, null, 2]))).toBe(2)
   })
 })
