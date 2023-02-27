a1bea9060d0137b0fd4dfeca610e69723446f41d
danielzhang183 <danielzhang182@gmail.com>
2023-02-27 18:35:06 +0800

    question: tree 257 easy

--- packages/code/test/structures/tree/257.test.ts
+++ packages/code/test/structures/tree/257.test.ts
@@ -1,8 +1,10 @@
 import { describe, expect, it } from 'vitest'
 import { binaryTreePaths } from '../../../src/structures/tree/257'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('binaryTreePaths', () => {
   it('exported', () => {
-    expect(binaryTreePaths([1, 2, 3, null, 5])).toBe([1])
+    expect(binaryTreePaths(createBinaryTree([1, 2, 3, null, 5]))).toStrictEqual(['1->2->5', '1->3'])
+    expect(binaryTreePaths(createBinaryTree([1]))).toStrictEqual(['1'])
   })
 })
