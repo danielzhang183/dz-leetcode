7305fa7554d9d8ffb92fd578a31bcb1819323590
danielzhang183 <danielzhang182@gmail.com>
2023-02-27 19:32:08 +0800

    question: tree 404 easy

--- packages/code/test/structures/tree/404.test.ts
+++ packages/code/test/structures/tree/404.test.ts
@@ -1,8 +1,10 @@
 import { describe, expect, it } from 'vitest'
 import { sumOfLeftLeaves } from '../../../src/structures/tree/404'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('sumOfLeftLeaves', () => {
   it('exported', () => {
-    expect(sumOfLeftLeaves([3, 9, 20, null, null, 15, 7])).toBe([1])
+    expect(sumOfLeftLeaves(createBinaryTree([3, 9, 20, null, null, 15, 7]))).toBe(24)
+    expect(sumOfLeftLeaves(createBinaryTree([1]))).toBe(0)
   })
 })
