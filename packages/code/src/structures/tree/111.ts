6e21578f9ad85319edeadc5faf66a6430c270b5e
danielzhang183 <danielzhang182@gmail.com>
2023-02-27 13:42:02 +0800

    question: tree 111 easy

--- packages/code/src/structures/tree/111.ts
+++ packages/code/src/structures/tree/111.ts
@@ -1,17 +1,44 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import type { TreeNode } from '../../utils'
+import { Queue } from '../../utils/queue'
+
+// Recursive
+export function minDepth1(root: TreeNode | null): number {
+  if (!root)
+    return 0
+  if (!root.left && !root.right)
+    return 1
+
+  let min = Number.MAX_SAFE_INTEGER
+  if (root.left)
+    min = Math.min(min, minDepth(root.left))
+  if (root.right)
+    min = Math.min(min, minDepth(root.right))
+
+  return min + 1
+}
+
+// BFS - Queue
+class DepthNode {
+  constructor(public node: TreeNode, public depth = 0) {
+  }
+}
 
 export function minDepth(root: TreeNode | null): number {
+  if (!root)
+    return 0
 
+  const q = new Queue<DepthNode>()
+  q.enqueue(new DepthNode(root, 1))
+
+  while (!q.isEmpty()) {
+    const { node, depth } = q.dequeue()!
+    if (node.left == null && node.right == null)
+      return depth
+    if (node.left != null)
+      q.enqueue(new DepthNode(node.left, depth + 1))
+    if (node.right != null)
+      q.enqueue(new DepthNode(node.right, depth + 1))
+  }
+
+  return 0
 }
--- packages/code/test/structures/tree/111.test.ts
+++ packages/code/test/structures/tree/111.test.ts
@@ -1,8 +1,11 @@
 import { describe, expect, it } from 'vitest'
 import { minDepth } from '../../../src/structures/tree/111'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('minDepth', () => {
   it('exported', () => {
-    expect(minDepth([3, 9, 20, null, null, 15, 7])).toBe([2, null, 3, null, 4, null, 5, null, 6])
+    expect(minDepth(createBinaryTree([3]))).toBe(1)
+    expect(minDepth(createBinaryTree([3, 9, 20, null, null, 15, 7]))).toBe(2)
+    expect(minDepth(createBinaryTree([2, null, 3, null, 4, null, 5, null, 6]))).toBe(5)
   })
 })
