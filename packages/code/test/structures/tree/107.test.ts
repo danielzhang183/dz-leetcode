1e528b11e05f4befdd508e6d9787d307cab0f569
danielzhang183 <danielzhang182@gmail.com>
2023-02-28 17:43:04 +0800

    question: tree 107 medium

--- packages/code/test/structures/tree/107.test.ts
+++ packages/code/test/structures/tree/107.test.ts
@@ -1,8 +1,11 @@
 import { describe, expect, it } from 'vitest'
 import { levelOrderBottom } from '../../../src/structures/tree/107'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('levelOrderBottom', () => {
   it('exported', () => {
-    expect(levelOrderBottom([3, 9, 20, null, null, 15, 7])).toBe([1])
+    expect(levelOrderBottom(createBinaryTree([3, 9, 20, null, null, 15, 7]))).toStrictEqual([[15, 7], [9, 20], [3]])
+    expect(levelOrderBottom(createBinaryTree([1]))).toStrictEqual([[1]])
+    expect(levelOrderBottom(createBinaryTree([]))).toStrictEqual([])
   })
 })
