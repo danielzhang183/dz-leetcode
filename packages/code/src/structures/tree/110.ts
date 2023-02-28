58886fee6e01f55ea6466e725d2623845ae03cff
danielzhang183 <danielzhang182@gmail.com>
2023-02-28 13:37:04 +0800

    question: tree 110 easy

--- packages/code/src/structures/tree/110.ts
+++ packages/code/src/structures/tree/110.ts
@@ -1,17 +1,38 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import type { TreeNode } from '../../utils'
 
+// DFS
+export function isBalanced1(root: TreeNode | null): boolean {
+  if (root == null)
+    return true
+
+  const height = (root: TreeNode | null): number => {
+    if (root == null)
+      return 0
+    return Math.max(height(root.left), height(root.right)) + 1
+  }
+
+  return Math.abs(height(root.left) - height(root.right)) <= 1
+    && isBalanced(root.left)
+    && isBalanced(root.right)
+}
+
+// DFS - bottom to top
 export function isBalanced(root: TreeNode | null): boolean {
+  const height = (root: TreeNode | null): number => {
+    if (root == null)
+      return 0
 
+    const leftHeight = height(root.left)
+    if (leftHeight === -1)
+      return -1
+    const rightHeight = height(root.right)
+    if (rightHeight === -1)
+      return -1
+
+    return Math.abs(leftHeight - rightHeight) < 2
+      ? Math.max(leftHeight, rightHeight) + 1
+      : -1
+  }
+
+  return height(root) !== -1
 }
--- packages/code/test/structures/tree/110.test.ts
+++ packages/code/test/structures/tree/110.test.ts
@@ -1,8 +1,11 @@
 import { describe, expect, it } from 'vitest'
 import { isBalanced } from '../../../src/structures/tree/110'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('isBalanced', () => {
   it('exported', () => {
-    expect(isBalanced([3, 9, 20, null, null, 15, 7])).toBe([1, 2, 2, 3, 3, null, null, 4, 4])
+    expect(isBalanced(createBinaryTree([3, 9, 20, null, null, 15, 7]))).toBe(true)
+    expect(isBalanced(createBinaryTree([1, 2, 2, 3, 3, null, null, 4, 4]))).toBe(false)
+    expect(isBalanced(createBinaryTree([]))).toBe(true)
   })
 })
