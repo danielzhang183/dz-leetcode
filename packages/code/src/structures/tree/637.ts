c437192b0c7a4626af39ebac60ef496ff377c949
danielzhang183 <danielzhang182@gmail.com>
2023-02-28 08:05:15 +0800

    question: tree 637 easy

--- packages/code/src/structures/tree/637.ts
+++ packages/code/src/structures/tree/637.ts
@@ -1,17 +1,61 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import type { TreeNode } from '../../utils'
 
+// DFS
+export function averageOfLevels1(root: TreeNode | null): number[] {
+  const sums = new Map<number, number>()
+  const counts = new Map<number, number>()
+  const dfs = (
+    root: TreeNode | null,
+    level: number,
+    sums: Map<number, number>,
+    counts: Map<number, number>,
+  ) => {
+    if (root == null)
+      return
+
+    if (level < sums.size) {
+      sums.set(level, sums.get(level)! + root.val!)
+      counts.set(level, counts.get(level)! + 1)
+    }
+    else {
+      sums.set(level, root.val!)
+      counts.set(level, 1)
+    }
+    dfs(root.left, level + 1, sums, counts)
+    dfs(root.right, level + 1, sums, counts)
+  }
+  dfs(root, 0, sums, counts)
+
+  const averages: number[] = []
+  for (let i = 0; i < sums.size; i++)
+    averages.push(sums.get(i)! / counts.get(i)!)
+
+  return averages
+}
+
+// BFS - Stack
 export function averageOfLevels(root: TreeNode | null): number[] {
+  if (root == null)
+    return []
 
+  const ans: number[] = []
+  const stack = [root]
+  while (stack.length) {
+    let sum = 0
+    const size = stack.length
+
+    for (let i = 0; i < size; i++) {
+      const curr = stack.shift()!
+      sum += curr.val ?? 0
+      if (curr.left)
+        stack.push(curr.left)
+
+      if (curr.right)
+        stack.push(curr.right)
+    }
+
+    ans.push(sum / size)
+  }
+
+  return ans
 }
--- packages/code/test/structures/tree/637.test.ts
+++ packages/code/test/structures/tree/637.test.ts
@@ -1,8 +1,13 @@
 import { describe, expect, it } from 'vitest'
 import { averageOfLevels } from '../../../src/structures/tree/637'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('averageOfLevels', () => {
   it('exported', () => {
-    expect(averageOfLevels([3, 9, 20, null, null, 15, 7])).toBe([3, 9, 20, 15, 7])
+    expect(averageOfLevels(createBinaryTree([3, 9, 20, null, null, 15, 7]))).toStrictEqual([3.00000, 14.50000, 11.00000])
+    expect(averageOfLevels(createBinaryTree([3, 9, 20, 15, 7]))).toStrictEqual([3.00000, 14.50000, 11.00000])
+    expect(averageOfLevels(createBinaryTree([3, 1, 5, 0, 2, 4, 6]))).toStrictEqual([3.00000, 3.00000, 3.00000])
+    expect(averageOfLevels(createBinaryTree([98, 97, null, 88, null, 84, null, 79, 87, 64, null, null, null, 63, 69, 62, null, null, null, 30, null, 27, 59, 9, null, null, null, 3, null, 0, null, -4, null, -16, null, -18, -7, -19, null, null, null, -23, null, -34, null, -42, null, -59, null, -63, null, -64, null, -69, null, -75, null, -81])))
+      .toStrictEqual([98.00000, 97.00000, 88.00000, 84.00000, 83.00000, 64.00000, 66.00000, 62.00000, 30.00000, 43.00000, 9.00000, 3.00000, 0.00000, -4.00000, -16.00000, -12.50000, -19.00000, -23.00000, -34.00000, -42.00000, -59.00000, -63.00000, -64.00000, -69.00000, -75.00000, -81.00000])
   })
 })
