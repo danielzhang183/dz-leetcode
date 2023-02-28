7ea062f4300cc33ab4fec456683098d34878cd08
danielzhang183 <danielzhang182@gmail.com>
2023-02-28 15:21:12 +0800

    question: tree 098 medium

--- packages/code/src/structures/tree/098.ts
+++ packages/code/src/structures/tree/098.ts
@@ -1,17 +1,37 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import type { TreeNode } from '../../utils'
 
+// DFS
+export function isValidBST1(root: TreeNode | null): boolean {
+  const dfs = (root: TreeNode | null, lower: number, upper: number): boolean => {
+    if (root == null)
+      return true
+
+    if (lower >= root.val! || upper <= root.val!)
+      return false
+
+    return dfs(root.left, lower, root.val!)
+      && dfs(root.right, root.val!, upper)
+  }
+
+  return dfs(root, -Infinity, Infinity)
+}
+
+// Inorder Traversal
 export function isValidBST(root: TreeNode | null): boolean {
+  const stack = []
+  let prevVal = -Infinity
 
+  while (stack.length || root != null) {
+    while (root != null) {
+      stack.push(root)
+      root = root.left
+    }
+    root = stack.pop()!
+    if (root.val! <= prevVal)
+      return false
+    prevVal = root.val!
+    root = root.right
+  }
+
+  return true
 }
--- packages/code/test/structures/tree/098.test.ts
+++ packages/code/test/structures/tree/098.test.ts
@@ -1,8 +1,11 @@
 import { describe, expect, it } from 'vitest'
 import { isValidBST } from '../../../src/structures/tree/098'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('isValidBST', () => {
   it('exported', () => {
-    expect(isValidBST([2, 1, 3])).toBe([5, 1, 4, null, null, 3, 6])
+    expect(isValidBST(createBinaryTree([2, 1, 3]))).toBe(true)
+    expect(isValidBST(createBinaryTree([5, 1, 4, null, null, 3, 6]))).toBe(false)
+    expect(isValidBST(createBinaryTree([5, 4, 6, null, null, 3, 7]))).toBe(false)
   })
 })
