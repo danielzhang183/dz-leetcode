9a8eca5a3016fc441bcb6a0ec5550f83515d2b49
danielzhang183 <danielzhang182@gmail.com>
2023-02-28 10:38:59 +0800

    question: tree 226 easy

--- packages/code/src/structures/tree/226.ts
+++ packages/code/src/structures/tree/226.ts
@@ -1,17 +1,63 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import type { TreeNode } from '../../utils'
+import { Queue } from '../../utils/queue'
 
+// DFS
+export function invertTree2(root: TreeNode | null): TreeNode | null {
+  if (root == null)
+    return null
+
+  const left = invertTree(root.left)
+  const right = invertTree(root.right)
+  root.left = right
+  root.right = left
+
+  return root
+}
+
+// BFS
+// Queue
+export function invertTree1(root: TreeNode | null): TreeNode | null {
+  const q = new Queue<TreeNode | null>()
+  q.enqueue(root)
+
+  while (!q.isEmpty()) {
+    let size = q.size
+    while (size--) {
+      const curr = q.dequeue()!
+      if (curr == null)
+        continue
+
+      [curr.left, curr.right] = [curr.right, curr.left]
+
+      if (curr.left)
+        q.enqueue(curr.left)
+      if (curr.right)
+        q.enqueue(curr.right)
+    }
+  }
+
+  return root
+}
+
+// Stack
 export function invertTree(root: TreeNode | null): TreeNode | null {
+  const stack: (TreeNode | null)[] = [root]
 
+  while (stack.length) {
+    let size = stack.length
+    while (size--) {
+      const curr = stack.shift()!
+      if (curr == null)
+        continue
+
+      [curr.left, curr.right] = [curr.right, curr.left]
+
+      if (curr.left)
+        stack.push(curr.left)
+      if (curr.right)
+        stack.push(curr.right)
+    }
+  }
+
+  return root
 }
--- packages/code/test/structures/tree/226.test.ts
+++ packages/code/test/structures/tree/226.test.ts
@@ -1,8 +1,15 @@
 import { describe, expect, it } from 'vitest'
 import { invertTree } from '../../../src/structures/tree/226'
+import { covertBinaryTreeToArray, createBinaryTree } from '../../../src/utils'
 
 describe('invertTree', () => {
   it('exported', () => {
-    expect(invertTree([4, 2, 7, 1, 3, 6, 9])).toBe([2, 1, 3])
+    const ll1 = createBinaryTree([4, 2, 7, 1, 3, 6, 9])
+    expect(covertBinaryTreeToArray(invertTree(ll1))).toStrictEqual([4, 7, 2, 9, 6, 3, 1],
+    )
+    const ll2 = createBinaryTree([2, 1, 3])
+    expect(covertBinaryTreeToArray(invertTree(ll2))).toStrictEqual([2, 3, 1])
+    const ll3 = createBinaryTree([])
+    expect(covertBinaryTreeToArray(invertTree(ll3))).toStrictEqual([])
   })
 })
