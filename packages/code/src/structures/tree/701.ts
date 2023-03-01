08bfd9acf8a62f70f34a54fd2fa991b71934fa30
danielzhang183 <danielzhang182@gmail.com>
2023-03-01 17:47:23 +0800

    question: tree 701 medium

--- packages/code/src/structures/tree/701.ts
+++ packages/code/src/structures/tree/701.ts
@@ -1,17 +1,68 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import { TreeNode } from '../../utils'
 
+// DFS
+
+// worse
+export function insertIntoBST2(root: TreeNode | null, val: number): TreeNode | null {
+  const node = new TreeNode(val)
+
+  if (root == null)
+    return (root = node)
+
+  const dfs = (root: TreeNode, val: number) => {
+    if (root.val! > val) {
+      if (root.left)
+        dfs(root.left, val)
+      else
+        root.left = node
+    }
+    else {
+      if (root.right)
+        dfs(root.right, val)
+      else
+        root.right = node
+    }
+  }
+
+  dfs(root, val)
+  return root
+}
+
+// so much better
 export function insertIntoBST(root: TreeNode | null, val: number): TreeNode | null {
+  if (root == null)
+    return new TreeNode(val)
 
+  if (root.val! > val)
+    root.left = insertIntoBST(root.left, val)
+  if (root.val! < val)
+    root.right = insertIntoBST(root.right, val)
+
+  return root
+}
+
+// Traveral
+export function insertIntoBST1(root: TreeNode | null, val: number): TreeNode | null {
+  const node = new TreeNode(val)
+  if (root == null)
+    return (root = node)
+
+  let curr: TreeNode | null = root
+  let parent = curr
+  while (curr) {
+    parent = curr
+
+    if (curr.val! > val) {
+      curr = curr.left
+      if (curr == null)
+        parent.left = node
+    }
+    else {
+      curr = curr.right
+      if (curr == null)
+        parent.right = node
+    }
+  }
+
+  return root
 }
--- packages/code/test/structures/tree/701.test.ts
+++ packages/code/test/structures/tree/701.test.ts
@@ -1,10 +1,24 @@
 import { describe, expect, it } from 'vitest'
 import { insertIntoBST } from '../../../src/structures/tree/701'
+import { covertBinaryTreeToArray, createBinaryTree } from '../../../src/utils'
 
 describe('insertIntoBST', () => {
   it('exported', () => {
-    expect(insertIntoBST([4, 2, 7, 1, 3])).toBe(5)
-    expect(insertIntoBST([40, 20, 60, 10, 30, 50, 70])).toBe(25)
-    expect(insertIntoBST([4, 2, 7, 1, 3, null, null, null, null, null, null])).toBe(5)
+    expect(covertBinaryTreeToArray(
+      insertIntoBST(createBinaryTree([4, 2, 7, 1, 3]), 5),
+    )).toStrictEqual([4, 2, 7, 1, 3, 5])
+
+    expect(covertBinaryTreeToArray(
+      insertIntoBST(createBinaryTree([40, 20, 60, 10, 30, 50, 70]), 25),
+    )).toStrictEqual([40, 20, 60, 10, 30, 50, 70, 25])
+    // .toStrictEqual([40, 20, 60, 10, 30, 50, 70, null, null, 25])
+
+    expect(covertBinaryTreeToArray(
+      insertIntoBST(createBinaryTree([4, 2, 7, 1, 3, null, null, null, null, null, null]), 5),
+    )).toStrictEqual([4, 2, 7, 1, 3, 5])
+
+    expect(covertBinaryTreeToArray(
+      insertIntoBST(createBinaryTree([]), 5),
+    )).toStrictEqual([5])
   })
 })
