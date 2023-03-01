d505fafac0de8be5f81e3be46fdbd0ed807a4fb7
danielzhang183 <danielzhang182@gmail.com>
2023-03-01 16:47:42 +0800

    question: tree 199 medium

--- packages/code/test/structures/tree/199.test.ts
+++ packages/code/test/structures/tree/199.test.ts
@@ -1,8 +1,12 @@
 import { describe, expect, it } from 'vitest'
 import { rightSideView } from '../../../src/structures/tree/199'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('rightSideView', () => {
   it('exported', () => {
-    expect(rightSideView([1, 2, 3, null, 5, null, 4])).toBe([1, null, 3])
+    expect(rightSideView(createBinaryTree([1, 2, 3, null, 5, null, 4]))).toStrictEqual([1, 3, 4])
+    expect(rightSideView(createBinaryTree([1, null, 3]))).toStrictEqual([1, 3])
+    expect(rightSideView(createBinaryTree([1, 2, 3, 4]))).toStrictEqual([1, 3, 4])
+    expect(rightSideView(createBinaryTree([]))).toStrictEqual([])
   })
 })
