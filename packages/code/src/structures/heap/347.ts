b9398a47e4b9e7ee9c2480afaf7ec90363a63c17
danielzhang183 <danielzhang182@gmail.com>
2023-03-02 17:37:43 +0800

    question: heap 347 medium

--- packages/code/src/structures/heap/347.ts
+++ packages/code/src/structures/heap/347.ts
@@ -1,3 +1,11 @@
+// sort
 export function topKFrequent(nums: number[], k: number): number[] {
+  const countMap = new Map<number, number>()
+  for (let i = 0; i < nums.length; i++)
+    countMap.set(nums[i], (countMap.get(nums[i]) || 0) + 1)
 
+  return [...countMap.entries()]
+    .sort((a, b) => b[1] - a[1])
+    .splice(0, k)
+    .map(i => i[0])
 }
--- packages/code/test/structures/heap/347.test.ts
+++ packages/code/test/structures/heap/347.test.ts
@@ -3,7 +3,7 @@ import { topKFrequent } from '../../../src/structures/heap/347'
 
 describe('topKFrequent', () => {
   it('exported', () => {
-    expect(topKFrequent([1, 1, 1, 2, 2, 3])).toBe(2)
-    expect(topKFrequent([1])).toBe(1)
+    expect(topKFrequent([1, 1, 1, 2, 2, 3], 2)).toStrictEqual([1, 2])
+    expect(topKFrequent([1], 1)).toStrictEqual([1])
   })
 })
