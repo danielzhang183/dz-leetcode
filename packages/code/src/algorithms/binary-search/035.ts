4e511dabe6c78a6459bf2bf674e0fa99af746d18
danielzhang183 <danielzhang182@gmail.com>
2023-03-03 18:12:32 +0800

    question: bs 035 easy

--- packages/code/src/algorithms/binary-search/035.ts
+++ packages/code/src/algorithms/binary-search/035.ts
@@ -1,3 +1,16 @@
 export function searchInsert(nums: number[], target: number): number {
+  let l = 0
+  let r = nums.length - 1
 
+  while (l <= r) {
+    const mid = (l + r) >> 1
+    if (nums[mid] === target)
+      return mid
+    else if (nums[mid] < target)
+      l = mid + 1
+    else
+      r = mid - 1
+  }
+
+  return l
 }
--- packages/code/test/algorithms/binary-search/035.test.ts
+++ packages/code/test/algorithms/binary-search/035.test.ts
@@ -3,8 +3,8 @@ import { searchInsert } from '../../../src/algorithms/binary-search/035'
 
 describe('searchInsert', () => {
   it('exported', () => {
-    expect(searchInsert([1, 3, 5, 6])).toBe(5)
-    expect(searchInsert([1, 3, 5, 6])).toBe(2)
-    expect(searchInsert([1, 3, 5, 6])).toBe(7)
+    expect(searchInsert([1, 3, 5, 6], 5)).toBe(2)
+    expect(searchInsert([1, 3, 5, 6], 2)).toBe(1)
+    expect(searchInsert([1, 3, 5, 6], 7)).toBe(4)
   })
 })
