66441ee4f2133da0a77d5c00044df83e443a5699
danielzhang183 <danielzhang182@gmail.com>
2023-03-03 18:12:14 +0800

    question: bs 704 easy

--- packages/code/src/algorithms/binary-search/704.ts
+++ packages/code/src/algorithms/binary-search/704.ts
@@ -1,3 +1,16 @@
 export function search(nums: number[], target: number): number {
+  let l = 0
+  let r = nums.length
 
+  while (l <= r) {
+    const mid = (l + r) >> 1
+    if (nums[mid] === target)
+      return mid
+    else if (nums[mid] < target)
+      l = mid + 1
+    else
+      r = mid - 1
+  }
+
+  return -1
 }
--- packages/code/test/algorithms/binary-search/704.test.ts
+++ packages/code/test/algorithms/binary-search/704.test.ts
@@ -3,7 +3,7 @@ import { search } from '../../../src/algorithms/binary-search/704'
 
 describe('search', () => {
   it('exported', () => {
-    expect(search([-1, 0, 3, 5, 9, 12])).toBe(9)
-    expect(search([-1, 0, 3, 5, 9, 12])).toBe(2)
+    expect(search([-1, 0, 3, 5, 9, 12], 9)).toBe(4)
+    expect(search([-1, 0, 3, 5, 9, 12], 2)).toBe(-1)
   })
 })
