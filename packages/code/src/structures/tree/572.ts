da8fcac5045d04c12e943884635de35017031a0e
danielzhang183 <danielzhang182@gmail.com>
2023-03-03 13:15:08 +0800

    question: tree 572 easy

--- packages/code/src/structures/tree/572.ts
+++ packages/code/src/structures/tree/572.ts
@@ -1,17 +1,82 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import type { TreeNode } from '../../utils'
 
+// DFS
+export function isSubtree1(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+  const dfs = (p: TreeNode | null, q: TreeNode | null): boolean => {
+    if (p == null)
+      return false
+
+    return isSameTree(p, q) || dfs(p.left, q) || dfs(p.right, q)
+  }
+
+  return dfs(root, subRoot)
+}
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (p == null)
+    return q == null
+
+  return q != null
+    && p.val === q.val
+    && isSameTree(p.left, q.left)
+    && isSameTree(p.right, q.right)
+}
+
+// DFS + KMP
 export function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+  const pOrder: number[] = []
+  const qOrder: number[] = []
+  const maxElement = -Number.MAX_SAFE_INTEGER
+  const lNull = maxElement + 1
+  const rNull = maxElement + 2
 
+  function preorderTraversal(t: TreeNode | null, arr: number[]) {
+    if (t == null)
+      return
+
+    arr.push(t.val!)
+    if (t.left)
+      preorderTraversal(t.left, arr)
+    else
+      arr.push(lNull)
+
+    if (t.right)
+      preorderTraversal(t.right, arr)
+    else
+      arr.push(rNull)
+  }
+
+  preorderTraversal(root, pOrder)
+  preorderTraversal(subRoot, qOrder)
+
+  return kmp(pOrder, qOrder)
+}
+
+function kmp(p: number[], q: number[]) {
+  const pLen = p.length
+  const qLen = q.length
+  const fail: number[] = new Array(qLen).fill(-1)
+
+  for (let i = 1, j = -1; i < qLen; i++) {
+    while (j !== -1 && q[i] !== q[j + 1])
+      j = fail[j]
+
+    if (q[i] === q[j + 1])
+      ++j
+
+    fail[i] = j
+  }
+
+  for (let i = 0, j = -1; i < pLen; i++) {
+    while (j !== -1 && p[i] !== q[j + 1])
+      j = fail[j]
+
+    if (p[i] === q[j + 1])
+      ++j
+
+    if (j === qLen - 1)
+      return true
+  }
+
+  return false
 }
--- packages/code/test/structures/tree/572.test.ts
+++ packages/code/test/structures/tree/572.test.ts
@@ -1,9 +1,20 @@
 import { describe, expect, it } from 'vitest'
 import { isSubtree } from '../../../src/structures/tree/572'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('isSubtree', () => {
   it('exported', () => {
-    expect(isSubtree([3, 4, 5, 1, 2])).toBe([4, 1, 2])
-    expect(isSubtree([3, 4, 5, 1, 2, null, null, null, null, 0])).toBe([4, 1, 2])
+    expect(isSubtree(
+      createBinaryTree([3, 4, 5, 1, 2]),
+      createBinaryTree([4, 1, 2]),
+    )).toBe(true)
+    expect(isSubtree(
+      createBinaryTree([3, 4, 5, 1, 2, null, null, null, null, 0]),
+      createBinaryTree([4, 1, 2]),
+    )).toBe(false)
+    expect(isSubtree(
+      createBinaryTree([1, 1]),
+      createBinaryTree([1]),
+    )).toBe(true)
   })
 })
