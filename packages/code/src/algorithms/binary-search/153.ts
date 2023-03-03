c73fea10c3322c55012cb39ae0e8fcdacf006373
danielzhang183 <danielzhang182@gmail.com>
2023-03-03 18:43:52 +0800

    question: bs 153 easy

--- packages/code/src/algorithms/binary-search/153.ts
+++ packages/code/src/algorithms/binary-search/153.ts
@@ -1,3 +1,14 @@
 export function findMin(nums: number[]): number {
+  let l = 0
+  let r = nums.length - 1
 
+  while (l < r) {
+    const mid = (l + r) >> 1
+    if (nums[r] > nums[mid])
+      r = mid
+    else
+      l = mid + 1
+  }
+
+  return nums[l]
 }
--- packages/code/test/algorithms/binary-search/153.test.ts
+++ packages/code/test/algorithms/binary-search/153.test.ts
@@ -3,6 +3,8 @@ import { findMin } from '../../../src/algorithms/binary-search/153'
 
 describe('findMin', () => {
   it('exported', () => {
-    expect(findMin([3, 4, 5, 1, 2])).toBe([4, 5, 6, 7, 0, 1, 2])
+    expect(findMin([3, 4, 5, 1, 2])).toBe(1)
+    expect(findMin([4, 5, 6, 7, 0, 1, 2])).toBe(0)
+    expect(findMin([11, 13, 15, 17])).toBe(11)
   })
 })
