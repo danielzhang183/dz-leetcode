440bf4b642c8725c0a007e60a2eed403bde658cb
danielzhang183 <danielzhang182@gmail.com>
2023-03-06 07:26:06 +0800

    question: bs 017 medium

--- packages/code/test/algorithms/back-tracking/017.test.ts
+++ packages/code/test/algorithms/back-tracking/017.test.ts
@@ -3,6 +3,8 @@ import { letterCombinations } from '../../../src/algorithms/back-tracking/017'
 
 describe('letterCombinations', () => {
   it('exported', () => {
-    expect(letterCombinations('23')).toBe('')
+    expect(letterCombinations('23')).toStrictEqual(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'])
+    expect(letterCombinations('')).toStrictEqual([])
+    expect(letterCombinations('2')).toStrictEqual(['a', 'b', 'c'])
   })
 })
