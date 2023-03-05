3cf33375bf1925832c60d5c618cdb7d3a908991e
danielzhang183 <danielzhang182@gmail.com>
2023-03-06 07:26:06 +0800

    question: bs 017 medium

--- packages/code/src/algorithms/back-tracking/017.ts
+++ packages/code/src/algorithms/back-tracking/017.ts
@@ -1,3 +1,25 @@
 export function letterCombinations(digits: string): string[] {
+  if (digits === '')
+    return []
 
+  const map = new Map<number, string>([
+    [2, 'abc'],
+    [3, 'def'],
+    [4, 'ghi'],
+    [5, 'jkl'],
+    [6, 'mno'],
+    [7, 'pqrs'],
+    [8, 'tuv'],
+    [9, 'wxyz'],
+  ])
+
+  const ans: string[] = []
+  const path: string[] = []
+
+  const dfs = () => {
+
+  }
+
+  dfs()
+  return ans
 }
--- packages/code/test/algorithms/back-tracking/017.test.ts
+++ packages/code/test/algorithms/back-tracking/017.test.ts
@@ -3,6 +3,8 @@ import { letterCombinations } from '../../../src/algorithms/back-tracking/017'
 
 describe('letterCombinations', () => {
   it('exported', () => {
-    expect(letterCombinations('23')).toBe('')
+    expect(letterCombinations('23')).toBe(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'])
+    expect(letterCombinations('')).toBe([])
+    expect(letterCombinations('2')).toBe(['a', 'b', 'c'])
   })
 })
