e70127698fcea43d49a998958fc5d78d1dffc317
danielzhang183 <danielzhang182@gmail.com>
2023-03-06 22:37:46 +0800

    question: bs 093 medium

--- packages/code/src/algorithms/back-tracking/093.ts
+++ packages/code/src/algorithms/back-tracking/093.ts
@@ -1,3 +1,35 @@
 export function restoreIpAddresses(s: string): string[] {
+  const ans: string[] = []
+  const path: string[] = []
 
+  const isValidIp = (ip: string): boolean => {
+    if ((ip.length > 1 && ip.startsWith('0')) || Number(ip) > 255)
+      return false
+    return true
+  }
+
+  const dfs = (s: string, begin: number) => {
+    const length = s.length
+    if (begin === length && path.length === 4) {
+      ans.push(path.join('.'))
+      return
+    }
+
+    if (path.length === 4 || begin === length)
+      return
+
+    for (let i = begin; i < Math.min(length, begin + 3); i++) {
+      const ip = s.substring(begin, i + 1)
+      if (isValidIp(ip)) {
+        path.push(ip)
+        dfs(s, i + 1)
+        path.pop()
+      }
+    }
+  }
+
+  dfs(s, 0)
+  return ans
 }
+
+restoreIpAddresses('25525511135')
--- packages/code/test/algorithms/back-tracking/093.test.ts
+++ packages/code/test/algorithms/back-tracking/093.test.ts
@@ -3,6 +3,8 @@ import { restoreIpAddresses } from '../../../src/algorithms/back-tracking/093'
 
 describe('restoreIpAddresses', () => {
   it('exported', () => {
-    expect(restoreIpAddresses('25525511135')).toBe('0000')
+    expect(restoreIpAddresses('25525511135')).toStrictEqual(['255.255.11.135', '255.255.111.35'])
+    expect(restoreIpAddresses('0000')).toStrictEqual(['0.0.0.0'])
+    expect(restoreIpAddresses('101023')).toStrictEqual(['1.0.10.23', '1.0.102.3', '10.1.0.23', '10.10.2.3', '101.0.2.3'])
   })
 })
