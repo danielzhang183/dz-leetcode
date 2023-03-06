3ad20d12601d6def704e8a4cb91eaae81dbd7d04
danielzhang183 <danielzhang182@gmail.com>
2023-03-07 00:06:45 +0800

    question: bs 078 medium

--- packages/code/src/algorithms/back-tracking/078.ts
+++ packages/code/src/algorithms/back-tracking/078.ts
@@ -1,3 +1,24 @@
+// N-ary Tree
 export function subsets(nums: number[]): number[][] {
+  const ans: number[][] = []
+  const path: number[] = []
 
+  const dfs = (nums: number[], begin: number) => {
+    // console.log({ path })
+    ans.push([...path])
+    const len = nums.length
+    if (begin === len)
+      return
+
+    for (let i = begin; i < len; i++) {
+      path.push(nums[i])
+      dfs(nums, i + 1)
+      path.pop()
+    }
+  }
+
+  dfs(nums, 0)
+  return ans
 }
+
+subsets([1, 2, 3])
--- packages/code/test/algorithms/back-tracking/078.test.ts
+++ packages/code/test/algorithms/back-tracking/078.test.ts
@@ -3,6 +3,7 @@ import { subsets } from '../../../src/algorithms/back-tracking/078'
 
 describe('subsets', () => {
   it('exported', () => {
-    expect(subsets([1, 2, 3])).toBe([0])
+    expect(subsets([1, 2, 3])).toStrictEqual([[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]])
+    expect(subsets([0])).toStrictEqual([[], [0]])
   })
 })
