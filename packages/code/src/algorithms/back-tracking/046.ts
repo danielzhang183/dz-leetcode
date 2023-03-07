8a61707de5c701d2f13f835caff67800f2af8141
danielzhang183 <danielzhang182@gmail.com>
2023-03-07 13:47:27 +0800

    question: bs 046 medium

--- packages/code/src/algorithms/back-tracking/046.ts
+++ packages/code/src/algorithms/back-tracking/046.ts
@@ -1,3 +1,27 @@
 export function permute(nums: number[]): number[][] {
+  const ans: number[][] = []
+  const path: number[] = []
+  const set = new Set<number>()
 
+  const dfs = () => {
+    if (path.length === nums.length) {
+      ans.push([...path])
+      return
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      if (set.has(nums[i]))
+        continue
+      set.add(nums[i])
+      path.push(nums[i])
+      dfs()
+      path.pop()
+      set.delete(nums[i])
+    }
+  }
+
+  dfs()
+  return ans
 }
+
+permute([1, 2, 3])
--- packages/code/test/algorithms/back-tracking/046.test.ts
+++ packages/code/test/algorithms/back-tracking/046.test.ts
@@ -3,6 +3,11 @@ import { permute } from '../../../src/algorithms/back-tracking/046'
 
 describe('permute', () => {
   it('exported', () => {
-    expect(permute([1, 2, 3])).toBe([0, 1])
+    expect(permute([1, 2, 3])).toStrictEqual([
+      [1, 2, 3], [1, 3, 2], [2, 1, 3],
+      [2, 3, 1], [3, 1, 2], [3, 2, 1],
+    ])
+    expect(permute([0, 1])).toStrictEqual([[0, 1], [1, 0]])
+    expect(permute([1])).toStrictEqual([[1]])
   })
 })
