0de7236fb1749d75710ebd11553d75adfd8b0457
danielzhang183 <danielzhang182@gmail.com>
2023-03-07 10:33:49 +0800

    question: bs 090 medium

--- packages/code/src/algorithms/back-tracking/090.ts
+++ packages/code/src/algorithms/back-tracking/090.ts
@@ -1,3 +1,26 @@
 export function subsetsWithDup(nums: number[]): number[][] {
+  const ans: number[][] = []
+  const path: number[] = []
 
-};
+  const dfs = (begin: number) => {
+    ans.push([...path])
+
+    if (begin === nums.length)
+      return
+
+    for (let i = begin; i < nums.length; i++) {
+      // dimiss duplicate num (index > 1)
+      // you can also use set to remove duplicate num
+      if (i > begin && nums[i] === nums[i - 1])
+        continue
+      path.push(nums[i])
+      dfs(i + 1)
+      path.pop()
+    }
+  }
+
+  dfs(0)
+  return ans
+}
+
+// subsetsWithDup([1, 2, 2])
--- packages/code/test/algorithms/back-tracking/090.test.ts
+++ packages/code/test/algorithms/back-tracking/090.test.ts
@@ -3,6 +3,7 @@ import { subsetsWithDup } from '../../../src/algorithms/back-tracking/090'
 
 describe('subsetsWithDup', () => {
   it('exported', () => {
-    expect(subsetsWithDup([1,2,2])).toBe([0])
+    expect(subsetsWithDup([1, 2, 2])).toStrictEqual([[], [1], [1, 2], [1, 2, 2], [2], [2, 2]])
+    expect(subsetsWithDup([0])).toStrictEqual([[], [0]])
   })
 })
