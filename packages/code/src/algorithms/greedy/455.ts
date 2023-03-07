fb4eba9afc328f301375eacf53b8d84e91377983
danielzhang183 <danielzhang182@gmail.com>
2023-03-07 17:38:51 +0800

    question: greedy 455 easy

--- packages/code/src/algorithms/greedy/455.ts
+++ packages/code/src/algorithms/greedy/455.ts
@@ -1,3 +1,14 @@
 export function findContentChildren(g: number[], s: number[]): number {
+  g.sort((a, b) => a - b)
+  s.sort((a, b) => a - b)
+  let j = 0
+  let count = 0
+  for (let i = 0; i < s.length && j < g.length; i++) {
+    if (s[i] >= g[j]) {
+      count++
+      j++
+    }
+  }
 
+  return count
 }
--- packages/code/test/algorithms/greedy/455.test.ts
+++ packages/code/test/algorithms/greedy/455.test.ts
@@ -3,7 +3,8 @@ import { findContentChildren } from '../../../src/algorithms/greedy/455'
 
 describe('findContentChildren', () => {
   it('exported', () => {
-    expect(findContentChildren([1, 2, 3])).toBe([1, 1])
-    expect(findContentChildren([1, 2])).toBe([1, 2, 3])
+    expect(findContentChildren([1, 2, 3], [1, 1])).toBe(1)
+    expect(findContentChildren([1, 2], [1, 2, 3])).toBe(2)
+    expect(findContentChildren([10, 9, 8, 7], [5, 6, 7, 8])).toBe(2)
   })
 })
