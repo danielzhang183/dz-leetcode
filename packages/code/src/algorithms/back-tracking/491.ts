9a6b98b0847ccc91164d59fd18c5d0c2b1c710a9
danielzhang183 <danielzhang182@gmail.com>
2023-03-07 13:11:55 +0800

    question: bs 491 medium

--- packages/code/src/algorithms/back-tracking/491.ts
+++ packages/code/src/algorithms/back-tracking/491.ts
@@ -1,3 +1,28 @@
 export function findSubsequences(nums: number[]): number[][] {
+  const ans: number[][] = []
+  const path: number[] = []
 
-};
+  const dfs = (begin: number) => {
+    if (path.length > 1) {
+      // console.log({ path })
+      ans.push([...path])
+    }
+
+    const set = new Set<number>()
+    for (let i = begin; i < nums.length; i++) {
+      if ((path.length && nums[i] < path[path.length - 1]) || set.has(nums[i]))
+        continue
+
+      set.add(nums[i])
+      path.push(nums[i])
+      dfs(i + 1)
+      path.pop()
+    }
+  }
+
+  dfs(0)
+  // console.log({ ans })
+  return ans
+}
+
+findSubsequences([4, 6, 7, 7])
--- packages/code/test/algorithms/back-tracking/491.test.ts
+++ packages/code/test/algorithms/back-tracking/491.test.ts
@@ -3,6 +3,10 @@ import { findSubsequences } from '../../../src/algorithms/back-tracking/491'
 
 describe('findSubsequences', () => {
   it('exported', () => {
-    expect(findSubsequences([4,6,7,7])).toBe([4,4,3,2,1])
+    expect(findSubsequences([4, 6, 7, 7])).toStrictEqual([
+      [4, 6], [4, 6, 7], [4, 6, 7, 7], [4, 7],
+      [4, 7, 7], [6, 7], [6, 7, 7], [7, 7],
+    ])
+    expect(findSubsequences([4, 4, 3, 2, 1])).toStrictEqual([[4, 4]])
   })
 })
