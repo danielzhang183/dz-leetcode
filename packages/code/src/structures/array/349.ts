1d9061e462aa7cc37c51352f1bd8f1c0c873b946
danielzhang183 <danielzhang182@gmail.com>
2023-03-08 13:21:44 +0800

    question: array 349 easy

--- packages/code/src/structures/array/349.ts
+++ packages/code/src/structures/array/349.ts
@@ -1,3 +1,32 @@
-export function intersection(nums1: number[], nums2: number[]): number[] {
-
+// Set
+export function intersection1(nums1: number[], nums2: number[]): number[] {
+  const set = new Set(nums2)
+  return [...new Set(nums1)].filter(i => set.has(i))
 }
+
+// Sort + Two Pointers
+export function intersection(nums1: number[], nums2: number[]): number[] {
+  const arr1 = [...new Set(nums1)].sort()
+  const arr2 = [...new Set(nums2)].sort()
+  const ans: number[] = []
+
+  let i = 0
+  let j = 0
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] === arr2[j]) {
+      ans.push(arr1[i])
+      i++
+      j++
+    }
+    else if (arr1[i] < arr2[j]) {
+      i++
+    }
+    else {
+      j++
+    }
+  }
+
+  return ans
+}
+
+intersection([4, 9, 5], [9, 4, 9, 8, 4])
--- packages/code/test/structures/array/349.test.ts
+++ packages/code/test/structures/array/349.test.ts
@@ -3,7 +3,7 @@ import { intersection } from '../../../src/structures/array/349'
 
 describe('intersection', () => {
   it('exported', () => {
-    expect(intersection([1, 2, 2, 1])).toBe([2, 2])
-    expect(intersection([4, 9, 5])).toBe([9, 4, 9, 8, 4])
+    expect(intersection([1, 2, 2, 1], [2, 2])).toStrictEqual([2])
+    expect(intersection([4, 9, 5], [9, 4, 9, 8, 4])).toStrictEqual([4, 9])
   })
 })
