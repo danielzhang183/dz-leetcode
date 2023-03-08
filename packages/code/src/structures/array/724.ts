1506e98e75d43e8a4e365c2d6aeff16777998dca
danielzhang183 <danielzhang182@gmail.com>
2023-03-08 10:19:09 +0800

    question: array 724 easy

--- packages/code/src/structures/array/724.ts
+++ packages/code/src/structures/array/724.ts
@@ -1,3 +1,14 @@
+// Prefix Sum
 export function pivotIndex(nums: number[]): number {
+  const total = nums.reduce((a, b) => a + b, 0)
+  let sum = 0
 
-};
+  for (let i = 0; i < nums.length; i++) {
+    if (2 * sum + nums[i] === total)
+      return i
+
+    sum += nums[i]
+  }
+
+  return -1
+}
--- packages/code/test/structures/array/724.test.ts
+++ packages/code/test/structures/array/724.test.ts
@@ -3,6 +3,8 @@ import { pivotIndex } from '../../../src/structures/array/724'
 
 describe('pivotIndex', () => {
   it('exported', () => {
-    expect(pivotIndex([1,7,3,6,5,6])).toBe([1,2,3])
+    expect(pivotIndex([1, 7, 3, 6, 5, 6])).toBe(3)
+    expect(pivotIndex([1, 2, 3])).toBe(-1)
+    expect(pivotIndex([2, 1, -1])).toBe(0)
   })
 })
