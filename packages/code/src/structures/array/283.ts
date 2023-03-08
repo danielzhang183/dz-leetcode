761401d5823f9f562c95067bd3f44ce03e5229eb
danielzhang183 <danielzhang182@gmail.com>
2023-03-08 12:43:53 +0800

    question: array 283 easy

--- packages/code/src/structures/array/283.ts
+++ packages/code/src/structures/array/283.ts
@@ -1,6 +1,13 @@
-export /**
- Do not return anything, modify nums in-place instead.
- */
-function moveZeroes(nums: number[]): void {
+export function moveZeroes(nums: number[]): void {
+  let i = 0
+  let j = 0
+  while (i < nums.length) {
+    if (nums[i] !== 0) {
+      [nums[j], nums[i]] = [nums[i], nums[j]]
+      j++
+    }
+    i++
+  }
+}
 
-};
+moveZeroes([0, 1, 0, 3, 12])
--- packages/code/test/structures/array/283.test.ts
+++ packages/code/test/structures/array/283.test.ts
@@ -3,6 +3,11 @@ import { moveZeroes } from '../../../src/structures/array/283'
 
 describe('moveZeroes', () => {
   it('exported', () => {
-    expect(moveZeroes([0,1,0,3,12])).toBe([0])
+    const a1 = [0, 1, 0, 3, 12]
+    moveZeroes(a1)
+    expect(a1).toStrictEqual([1, 3, 12, 0, 0])
+    const a2 = [0]
+    moveZeroes(a2)
+    expect(a2).toStrictEqual([0])
   })
 })
