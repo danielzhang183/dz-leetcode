0ff8558d0e72b9e560779966a63d3cb430ea6f6f
danielzhang183 <danielzhang182@gmail.com>
2023-03-09 14:11:59 +0800

    question: greedy 860 easy

--- packages/code/src/algorithms/greedy/860.ts
+++ packages/code/src/algorithms/greedy/860.ts
@@ -1,3 +1,33 @@
 export function lemonadeChange(bills: number[]): boolean {
+  let count5 = 0
+  let count10 = 0
 
+  for (let i = 0; i < bills.length; i++) {
+    if (bills[i] === 5) {
+      count5++
+    }
+    else if (bills[i] === 10) {
+      if (count5 > 0) {
+        count10++
+        count5--
+      }
+      else {
+        return false
+      }
+    }
+    else {
+      if (count10 > 0 && count5 > 0) {
+        count10--
+        count5--
+      }
+      else if (count5 >= 3) {
+        count5 -= 3
+      }
+      else {
+        return false
+      }
+    }
+  }
+
+  return true
 }
--- packages/code/test/algorithms/greedy/860.test.ts
+++ packages/code/test/algorithms/greedy/860.test.ts
@@ -3,6 +3,7 @@ import { lemonadeChange } from '../../../src/algorithms/greedy/860'
 
 describe('lemonadeChange', () => {
   it('exported', () => {
-    expect(lemonadeChange([5, 5, 5, 10, 20])).toBe([5, 5, 10, 10, 20])
+    expect(lemonadeChange([5, 5, 5, 10, 20])).toBe(true)
+    expect(lemonadeChange([5, 5, 10, 10, 20])).toBe(false)
   })
 })
