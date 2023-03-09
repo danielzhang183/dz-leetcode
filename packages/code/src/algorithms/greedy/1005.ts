bb2736b302a21957d3ebc4ee82fe7650aa55cebc
danielzhang183 <danielzhang182@gmail.com>
2023-03-09 13:36:20 +0800

    question: greedy 1005 easy

--- packages/code/src/algorithms/greedy/1005.ts
+++ packages/code/src/algorithms/greedy/1005.ts
@@ -1,3 +1,16 @@
 export function largestSumAfterKNegations(nums: number[], k: number): number {
+  nums.sort((a, b) => Math.abs(b) - Math.abs(a))
+  let sum = 0
+  const len = nums.length
 
-};
+  for (let i = 0; i < len; i++) {
+    if (nums[i] < 0 && k > 0) {
+      nums[i] = -nums[i]
+      k--
+    }
+
+    sum += nums[i]
+  }
+
+  return sum + (k % 2 === 0 ? 0 : -2 * nums[len - 1])
+}
--- packages/code/test/algorithms/greedy/1005.test.ts
+++ packages/code/test/algorithms/greedy/1005.test.ts
@@ -3,8 +3,8 @@ import { largestSumAfterKNegations } from '../../../src/algorithms/greedy/1005'
 
 describe('largestSumAfterKNegations', () => {
   it('exported', () => {
-    expect(largestSumAfterKNegations([4,2,3])).toBe(1)
-    expect(largestSumAfterKNegations([3,-1,0,2])).toBe(3)
-    expect(largestSumAfterKNegations([2,-3,-1,5,-4])).toBe(2)
+    expect(largestSumAfterKNegations([4, 2, 3], 1)).toBe(5)
+    expect(largestSumAfterKNegations([3, -1, 0, 2], 3)).toBe(6)
+    expect(largestSumAfterKNegations([2, -3, -1, 5, -4], 2)).toBe(13)
   })
 })
