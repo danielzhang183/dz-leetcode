ad23fd79ee0bb50cd4343174352ce653d2591c1a
danielzhang183 <danielzhang182@gmail.com>
2023-03-10 14:10:43 +0800

    question: greedy 738 easy

--- packages/code/src/algorithms/greedy/738.ts
+++ packages/code/src/algorithms/greedy/738.ts
@@ -1,3 +1,19 @@
 export function monotoneIncreasingDigits(n: number): number {
+  const arr = String(n).split('').map(Number)
+  const len = arr.length
+  let flag = len
 
-};
+  for (let i = len - 2; i >= 0; i--) {
+    if (arr[i] > arr[i + 1]) {
+      arr[i] -= 1
+      flag = i + 1
+    }
+  }
+
+  for (let i = flag; i < len; i++)
+    arr[i] = 9
+
+  return Number(arr.join(''))
+}
+
+monotoneIncreasingDigits(10)
--- packages/code/test/algorithms/greedy/738.test.ts
+++ packages/code/test/algorithms/greedy/738.test.ts
@@ -3,6 +3,8 @@ import { monotoneIncreasingDigits } from '../../../src/algorithms/greedy/738'
 
 describe('monotoneIncreasingDigits', () => {
   it('exported', () => {
-    expect(monotoneIncreasingDigits(10)).toBe(1234)
+    expect(monotoneIncreasingDigits(10)).toBe(9)
+    expect(monotoneIncreasingDigits(1234)).toBe(1234)
+    expect(monotoneIncreasingDigits(332)).toBe(299)
   })
 })
