dd309196e1976e480e997aae06e0bf972986323b
danielzhang183 <danielzhang182@gmail.com>
2023-03-10 15:12:10 +0800

    question: greedy 122 medium

--- packages/code/src/algorithms/greedy/122.ts
+++ packages/code/src/algorithms/greedy/122.ts
@@ -1,3 +1,7 @@
 export function maxProfit(prices: number[]): number {
+  let ans = 0
+  for (let i = 1; i < prices.length; i++)
+    ans += Math.max(prices[i] - prices[i - 1], 0)
 
-};
+  return ans
+}
