a76979d7f9c0b19c1027afe83217d8784ce11480
danielzhang183 <danielzhang182@gmail.com>
2023-03-10 17:58:19 +0800

    question: greedy 135 hard

--- packages/code/src/algorithms/greedy/135.ts
+++ packages/code/src/algorithms/greedy/135.ts
@@ -1,3 +1,44 @@
-export function candy(ratings: number[]): number {
+export function candy1(ratings: number[]): number {
+  const len = ratings.length
+  const candy: number[] = new Array(len).fill(1)
 
-};
+  for (let i = 1; i < len; i++) {
+    if (ratings[i] > ratings[i - 1])
+      candy[i] = candy[i - 1] + 1
+  }
+
+  for (let i = len - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1])
+      candy[i] = Math.max(candy[i], candy[i + 1] + 1)
+  }
+
+  return candy.reduce((a, b) => a + b, 0)
+}
+
+export function candy(ratings: number[]) {
+  let candy = 1
+  let inc = 1
+  let dec = 0
+  let pre = 1
+
+  for (let i = 1; i < ratings.length; i++) {
+    if (ratings[i] >= ratings[i - 1]) {
+      dec = 0
+      if (ratings[i] === ratings[i - 1])
+        pre = 1
+      else
+        pre++
+      candy += pre
+      inc = pre
+    }
+    else {
+      dec++
+      if (dec === inc)
+        dec++
+
+      candy += dec
+      pre = 1
+    }
+  }
+  return candy
+}
--- packages/code/test/algorithms/greedy/135.test.ts
+++ packages/code/test/algorithms/greedy/135.test.ts
@@ -3,6 +3,8 @@ import { candy } from '../../../src/algorithms/greedy/135'
 
 describe('candy', () => {
   it('exported', () => {
-    expect(candy([1,0,2])).toBe([1,2,2])
+    expect(candy([1, 0, 2])).toBe(5)
+    expect(candy([1, 2, 2])).toBe(4)
+    expect(candy([1, 2, 2, 5, 4, 3, 2])).toBe(14)
   })
 })
