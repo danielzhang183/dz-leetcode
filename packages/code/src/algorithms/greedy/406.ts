8a9708ffa9bed63f92533df02a0a04e57ce9f127
danielzhang183 <danielzhang182@gmail.com>
2023-03-12 13:22:17 +0800

    question: greedy 406 medium

--- packages/code/src/algorithms/greedy/406.ts
+++ packages/code/src/algorithms/greedy/406.ts
@@ -1,3 +1,11 @@
 export function reconstructQueue(people: number[][]): number[][] {
+  people.sort((a, b) => (b[0] - a[0]) || (a[1] - b[1]))
 
-};
+  const ans: number[][] = []
+  for (let i = 0; i < people.length; i++)
+    ans.splice(people[i][1], 0, people[i])
+
+  return ans
+}
+
+reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]])
--- packages/code/test/algorithms/greedy/406.test.ts
+++ packages/code/test/algorithms/greedy/406.test.ts
@@ -3,6 +3,11 @@ import { reconstructQueue } from '../../../src/algorithms/greedy/406'
 
 describe('reconstructQueue', () => {
   it('exported', () => {
-    expect(reconstructQueue([[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]])).toBe([[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]])
+    expect(reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]))
+      .toBe([[5, 0], [7, 0], [5, 2], [6, 1], [4, 4], [7, 1]])
+    expect(reconstructQueue([[6, 0], [5, 0], [4, 0], [3, 2], [2, 2], [1, 4]]))
+      .toBe([[4, 0], [5, 0], [2, 2], [3, 2], [1, 4], [6, 0]])
+    expect(reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]))
+      .toBe([[5, 0], [7, 0], [5, 2], [6, 1], [4, 4], [7, 1]])
   })
 })
