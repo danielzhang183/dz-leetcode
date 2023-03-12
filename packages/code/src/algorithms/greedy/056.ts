968a2e6cfc22dd329b40ab64d966fb158e6f199f
danielzhang183 <danielzhang182@gmail.com>
2023-03-12 22:54:15 +0800

    question: greedy 56 medium

--- packages/code/src/algorithms/greedy/056.ts
+++ packages/code/src/algorithms/greedy/056.ts
@@ -1,3 +1,18 @@
 export function merge(intervals: number[][]): number[][] {
+  if (intervals.length < 2)
+    return intervals
 
-};
+  intervals.sort((a, b) => a[0] - b[0])
+  const ans: number[][] = [intervals[0]]
+
+  for (let i = 1; i < intervals.length; i++) {
+    const curr = intervals[i]
+    const right = ans[ans.length - 1][1]
+    if (curr[0] <= right)
+      ans[ans.length - 1][1] = Math.max(right, curr[1])
+    else
+      ans.push(curr)
+  }
+
+  return ans
+}
--- packages/code/test/algorithms/greedy/056.test.ts
+++ packages/code/test/algorithms/greedy/056.test.ts
@@ -3,6 +3,7 @@ import { merge } from '../../../src/algorithms/greedy/056'
 
 describe('merge', () => {
   it('exported', () => {
-    expect(merge([[1,3],[2,6],[8,10],[15,18]])).toBe([[1,4],[4,5]])
+    expect(merge([[1, 3], [2, 6], [8, 10], [15, 18]])).toStrictEqual([[1, 6], [8, 10], [15, 18]])
+    expect(merge([[1, 4], [4, 5]])).toStrictEqual([[1, 5]])
   })
 })
