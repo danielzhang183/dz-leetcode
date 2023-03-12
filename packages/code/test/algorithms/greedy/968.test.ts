20e3becb99a9d0edfc46ed45bcc14a81c12ec84a
danielzhang183 <danielzhang182@gmail.com>
2023-03-13 07:00:32 +0800

    question: greedy 968 hard

--- packages/code/test/algorithms/greedy/968.test.ts
+++ packages/code/test/algorithms/greedy/968.test.ts
@@ -1,8 +1,10 @@
 import { describe, expect, it } from 'vitest'
 import { minCameraCover } from '../../../src/algorithms/greedy/968'
+import { createBinaryTree } from '../../../src/utils'
 
 describe('minCameraCover', () => {
   it('exported', () => {
-    expect(minCameraCover([0,0,null,0,0])).toBe([0,0,null,0,null,0,null,null,0])
+    expect(minCameraCover(createBinaryTree([0, 0, null, 0, 0]))).toBe(1)
+    expect(minCameraCover(createBinaryTree([0, 0, null, 0, null, 0, null, null, 0]))).toBe(2)
   })
 })
