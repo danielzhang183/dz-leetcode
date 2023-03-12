b304791bf634473c94610e021cb444ab5364e7f9
danielzhang183 <danielzhang182@gmail.com>
2023-03-12 21:13:26 +0800

    question: greedy 435 medium

--- packages/code/src/algorithms/greedy/435.ts
+++ packages/code/src/algorithms/greedy/435.ts
@@ -1,3 +1,38 @@
-export function eraseOverlapIntervals(intervals: number[][]): number {
+// sort by right
+export function eraseOverlapIntervals1(intervals: number[][]): number {
+  intervals.sort((a, b) => a[1] - b[1])
 
+  let count = 0
+  let right = intervals[0][1]
+  const len = intervals.length
+  for (let i = 1; i < len; i++) {
+    if (intervals[i][0] >= right) {
+      count++
+      right = intervals[i][1]
+    }
+  }
+
+  return len - count - 1
 }
+
+// sort by left
+export function eraseOverlapIntervals(intervals: number[][]): number {
+  intervals.sort((a, b) => a[0] - b[0])
+
+  let count = 0
+  let right = intervals[0][1]
+  for (let i = 1; i < intervals.length; i++) {
+    const tmp = intervals[i]
+    if (tmp[0] >= right) {
+      right = tmp[1]
+    }
+    else {
+      right = Math.min(right, tmp[1])
+      count++
+    }
+  }
+
+  return count
+}
+
+eraseOverlapIntervals([[1, 100], [11, 22], [1, 11], [2, 12]])
--- packages/code/test/algorithms/greedy/435.test.ts
+++ packages/code/test/algorithms/greedy/435.test.ts
@@ -3,6 +3,9 @@ import { eraseOverlapIntervals } from '../../../src/algorithms/greedy/435'
 
 describe('eraseOverlapIntervals', () => {
   it('exported', () => {
-    expect(eraseOverlapIntervals([[1, 2], [2, 3], [3, 4], [1, 3]])).toBe([[1, 2], [1, 2], [1, 2]])
+    expect(eraseOverlapIntervals([[1, 2], [2, 3], [3, 4], [1, 3]])).toBe(1)
+    expect(eraseOverlapIntervals([[1, 2], [1, 2], [1, 2]])).toBe(2)
+    expect(eraseOverlapIntervals([[1, 2], [2, 3]])).toBe(0)
+    expect(eraseOverlapIntervals([[1, 100], [11, 22], [1, 11], [2, 12]])).toBe(2)
   })
 })
