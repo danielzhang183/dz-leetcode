bf51531ca33662232eeab18ca34535abaf53201c
danielzhang183 <danielzhang182@gmail.com>
2023-03-12 15:47:44 +0800

    question: greedy 055 medium

--- packages/code/src/algorithms/greedy/055.ts
+++ packages/code/src/algorithms/greedy/055.ts
@@ -1,3 +1,18 @@
 export function canJump(nums: number[]): boolean {
+  const len = nums.length
+  let max = 0
 
+  for (let i = 0; i < len; i++) {
+    if (i <= max) {
+      max = Math.max(nums[i] + i, max)
+
+      if (max >= len - 1)
+        return true
+    }
+  }
+
+  return false
 }
+
+canJump([2, 3, 1, 1, 4])
+// canJump([3, 2, 1, 0, 4])
--- packages/code/test/algorithms/greedy/055.test.ts
+++ packages/code/test/algorithms/greedy/055.test.ts
@@ -3,6 +3,7 @@ import { canJump } from '../../../src/algorithms/greedy/055'
 
 describe('canJump', () => {
   it('exported', () => {
-    expect(canJump([2, 3, 1, 1, 4])).toBe([3, 2, 1, 0, 4])
+    expect(canJump([2, 3, 1, 1, 4])).toBe(true)
+    expect(canJump([3, 2, 1, 0, 4])).toBe(false)
   })
 })
