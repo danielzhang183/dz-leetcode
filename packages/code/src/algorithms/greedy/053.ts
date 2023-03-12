f54d5c18cd3a9a06ccd6cf643796ca004ab53926
danielzhang183 <danielzhang182@gmail.com>
2023-03-12 23:11:23 +0800

    question: greedy 053 medium

--- packages/code/src/algorithms/greedy/053.ts
+++ packages/code/src/algorithms/greedy/053.ts
@@ -1,3 +1,11 @@
 export function maxSubArray(nums: number[]): number {
-
-};
+  let sum = 0
+  let maxSoFar = 0
+  for (let i = 0; i < nums.length; i++) {
+    sum = Math.max(nums[i], sum + nums[i])
+    if (sum > maxSoFar)
+      maxSoFar = sum
+  }
+  return maxSoFar
+}
+maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
