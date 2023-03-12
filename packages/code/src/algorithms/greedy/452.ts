414d48a1267de19ae58d53a4fdf49f3f1d809f8a
danielzhang183 <danielzhang182@gmail.com>
2023-03-12 18:07:56 +0800

    question: greedy 452 medium

--- packages/code/src/algorithms/greedy/452.ts
+++ packages/code/src/algorithms/greedy/452.ts
@@ -1,3 +1,37 @@
-export function findMinArrowShots(points: number[][]): number {
+export function findMinArrowShots1(points: number[][]): number {
+  if (!points.length)
+    return 0
 
+  points.sort((a, b) => (a[0] - b[0]) || (a[1] - b[1]))
+
+  let count = 1
+  for (let i = 1; i < points.length; i++) {
+    if (points[i][0] <= points[i - 1][1])
+      points[i][1] = Math.min(points[i - 1][1], points[i][1])
+    else
+      count++
+  }
+
+  return count
 }
+
+export function findMinArrowShots(points: number[][]): number {
+  points.sort((a, b) => a[1] - b[1])
+
+  const n = points.length
+  let count = 0
+  let i = 0
+
+  while (i < n) {
+    const right = points[i][1]
+    i++
+
+    while (i < n && points[i][0] <= right)
+      i++
+    count++
+  }
+
+  return count
+}
+
+findMinArrowShots([[10, 16], [2, 8], [1, 6], [7, 12]])
--- packages/code/test/algorithms/greedy/452.test.ts
+++ packages/code/test/algorithms/greedy/452.test.ts
@@ -3,6 +3,8 @@ import { findMinArrowShots } from '../../../src/algorithms/greedy/452'
 
 describe('findMinArrowShots', () => {
   it('exported', () => {
-    expect(findMinArrowShots([[10, 16], [2, 8], [1, 6], [7, 12]])).toBe([[1, 2], [3, 4], [5, 6], [7, 8]])
+    expect(findMinArrowShots([[10, 16], [2, 8], [1, 6], [7, 12]])).toBe(2)
+    expect(findMinArrowShots([[1, 2], [3, 4], [5, 6], [7, 8]])).toBe(4)
+    expect(findMinArrowShots([[1, 2], [2, 3], [3, 4], [4, 5]])).toBe(2)
   })
 })
