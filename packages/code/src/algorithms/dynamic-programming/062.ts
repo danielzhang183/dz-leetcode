7cfe87997245f39bc516cb0fe81edf8ba05e901e
danielzhang183 <danielzhang182@gmail.com>
2023-03-13 13:08:43 +0800

    question: dp 062 medium

--- packages/code/src/algorithms/dynamic-programming/062.ts
+++ packages/code/src/algorithms/dynamic-programming/062.ts
@@ -1,3 +1,33 @@
+// DFS
 export function uniquePaths(m: number, n: number): number {
+  const dfs = (i: number, j: number): number => {
+    if (i > m || j > n)
+      return 0
 
+    if (i === m && j === n)
+      return 1
+
+    return dfs(i + 1, j) + dfs(i, j + 1)
+  }
+
+  return dfs(1, 1)
 }
+
+// DP
+// initial:  dp[i][0] = 1, dp[0][i] = 1
+// equation: dp[m][n] = dp[m - 1][n] + dp[m][n - 1]
+export function uniquePaths1(m: number, n: number): number {
+  const dp = Array.from(
+    { length: m },
+    () => Array.from({ length: n }).fill(1),
+  ) as number[][]
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++)
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+  }
+
+  return dp[m - 1][n - 1]
+}
+
+uniquePaths(3, 3)
--- packages/code/test/algorithms/dynamic-programming/062.test.ts
+++ packages/code/test/algorithms/dynamic-programming/062.test.ts
@@ -3,7 +3,12 @@ import { uniquePaths } from '../../../src/algorithms/dynamic-programming/062'
 
 describe('uniquePaths', () => {
   it('exported', () => {
-    expect(uniquePaths(3)).toBe(7)
-    expect(uniquePaths(3)).toBe(2)
+    expect(uniquePaths(2, 2)).toBe(2)
+    expect(uniquePaths(2, 3)).toBe(3)
+    expect(uniquePaths(2, 4)).toBe(4)
+    expect(uniquePaths(3, 2)).toBe(3)
+    expect(uniquePaths(3, 3)).toBe(6)
+    expect(uniquePaths(3, 4)).toBe(10)
+    expect(uniquePaths(3, 7)).toBe(28)
   })
 })
