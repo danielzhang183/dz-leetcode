ac870a8abd7eae88e659262d2d36c5bb3f7b4625
danielzhang183 <danielzhang182@gmail.com>
2023-03-13 09:34:13 +0800

    question: dp 509 easy

--- packages/code/src/algorithms/dynamic-programming/509.ts
+++ packages/code/src/algorithms/dynamic-programming/509.ts
@@ -1,3 +1,34 @@
-export function fib(n: number): number {
+// 滚动 array
+export function fib1(n: number): number {
+  if (n <= 1)
+    return n
 
+  let prev = 0
+  let cur = 1
+
+  for (let i = 2; i <= n; i++) {
+    const tmp = prev + cur
+    prev = cur
+    cur = tmp
+  }
+
+  return cur
+}
+
+// DFS
+export function fib2(n: number): number {
+  if (n <= 1)
+    return n
+
+  return fib(n - 1) + fib(n - 2)
+}
+
+// DP
+// dp[i] = dp[i - 1] + dp[i - 2]
+export function fib(n: number): number {
+  const dp: number[] = [0, 1]
+  for (let i = 2; i <= n; i++)
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+  return dp[n]
 }
--- packages/code/test/algorithms/dynamic-programming/509.test.ts
+++ packages/code/test/algorithms/dynamic-programming/509.test.ts
@@ -3,6 +3,10 @@ import { fib } from '../../../src/algorithms/dynamic-programming/509'
 
 describe('fib', () => {
   it('exported', () => {
-    expect(fib(2)).toBe(3)
+    expect(fib(0)).toBe(0)
+    expect(fib(1)).toBe(1)
+    expect(fib(2)).toBe(1)
+    expect(fib(3)).toBe(2)
+    expect(fib(4)).toBe(3)
   })
 })
