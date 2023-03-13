a61099cffc2ec5467586e2e5b84696a517aec20b
danielzhang183 <danielzhang182@gmail.com>
2023-03-13 10:58:01 +0800

    question: dp 746 easy

--- packages/code/src/algorithms/dynamic-programming/746.ts
+++ packages/code/src/algorithms/dynamic-programming/746.ts
@@ -1,3 +1,27 @@
-export function minCostClimbingStairs(cost: number[]): number {
+// DP
+// initial: i <= 2, Math.max(cost[0], cost[1])
+// equation: dp[i+1] = dp[i] + Math.max(c)
+export function minCostClimbingStairs1(cost: number[]): number {
+  const dp: number[] = [0, 0]
+  for (let i = 2; i <= cost.length; i++)
+    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
 
+  return dp.pop()!
 }
+
+// scroll array
+export function minCostClimbingStairs(cost: number[]): number {
+  let p = 0
+  let q = 0
+
+  for (let i = 2; i <= cost.length; i++) {
+    const tmp = Math.min(p + cost[i - 2], q + cost[i - 1])
+    p = q
+    q = tmp
+  }
+
+  return q
+}
+
+// minCostClimbingStairs([10, 15, 20])
+minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])
--- packages/code/test/algorithms/dynamic-programming/746.test.ts
+++ packages/code/test/algorithms/dynamic-programming/746.test.ts
@@ -3,6 +3,7 @@ import { minCostClimbingStairs } from '../../../src/algorithms/dynamic-programmi
 
 describe('minCostClimbingStairs', () => {
   it('exported', () => {
-    expect(minCostClimbingStairs([10, 15, 20])).toBe([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])
+    expect(minCostClimbingStairs([10, 15, 20])).toBe(15)
+    expect(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])).toBe(6)
   })
 })
