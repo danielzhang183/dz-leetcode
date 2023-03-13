d3864d7a61548af2df86be47d728d870aba3afb9
danielzhang183 <danielzhang182@gmail.com>
2023-03-13 15:19:36 +0800

    question: dp 343 medium

--- packages/code/src/algorithms/dynamic-programming/343.ts
+++ packages/code/src/algorithms/dynamic-programming/343.ts
@@ -1,3 +1,22 @@
+// DP
+// initial:  dp = [0, 0, 1..., 0]
+// equation: max(
+//    1 * dp[i - 1],   1 * (i - 1)
+//    2 * dp[i - 2],   2 * (i - 2)
+//    ...
+//  2/n * dp[i - 2/n], 2/n * (i - 2/n)
+// )
 export function integerBreak(n: number): number {
+  const dp: number[] = new Array(n + 1).fill(0)
+  dp[2] = 1
+  for (let i = 3; i <= n; i++) {
+    let max = 0
+    for (let j = 1; j <= (i >> 1); j++)
+      max = Math.max(max, j * dp[i - j], j * (i - j))
 
-};
+    dp[i] = max
+  }
+  return dp[n]
+}
+
+integerBreak(10)
--- packages/code/test/algorithms/dynamic-programming/343.test.ts
+++ packages/code/test/algorithms/dynamic-programming/343.test.ts
@@ -3,6 +3,8 @@ import { integerBreak } from '../../../src/algorithms/dynamic-programming/343'
 
 describe('integerBreak', () => {
   it('exported', () => {
-    expect(integerBreak(2)).toBe(10)
+    expect(integerBreak(2)).toBe(1 * 1)
+    expect(integerBreak(8)).toBe(2 * 9)
+    expect(integerBreak(10)).toBe(3 * 3 * 4)
   })
 })
