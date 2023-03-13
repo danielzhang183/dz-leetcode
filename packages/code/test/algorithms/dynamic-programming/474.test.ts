c63009612724b765c8ad8f6c3ae6bec3cca09a59
danielzhang183 <danielzhang182@gmail.com>
2023-03-14 00:15:10 +0800

    chore: fix

--- packages/code/test/algorithms/dynamic-programming/474.test.ts
+++ packages/code/test/algorithms/dynamic-programming/474.test.ts
@@ -3,8 +3,7 @@ import { findMaxForm } from '../../../src/algorithms/dynamic-programming/474'
 
 describe('findMaxForm', () => {
   it('exported', () => {
-    expect(findMaxForm(["10","0001","111001","1","0"])).toBe(5)
-    expect(findMaxForm(3)).toBe(["10","0","1"])
-    expect(findMaxForm(1)).toBe(1)
+    expect(findMaxForm(['10', '0001', '111001', '1', '0'], 5, 3)).toBe(5)
+    expect(findMaxForm(['10', '0', '1'], 3, 1)).toBe(2)
   })
 })
