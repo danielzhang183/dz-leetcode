37b027b5fb457cf136e327a9ff40439b626508f8
danielzhang183 <danielzhang182@gmail.com>
2023-03-13 09:57:21 +0800

    question: dp 070 easy

--- packages/code/src/algorithms/dynamic-programming/070.ts
+++ packages/code/src/algorithms/dynamic-programming/070.ts
@@ -1,3 +1,36 @@
-export function climbStairs(n: number): number {
+// DFS
+export function climbStairs1(n: number): number {
+  if (n < 3)
+    return n
 
-};
+  return climbStairs(n - 1) + climbStairs(n - 2)
+}
+
+// DP
+// inital: dp[0] = 0, dp[1] = 1, dp[2] = 2
+// equation: dp[i] = dp[i - 1] + dp[i - 2]
+export function climbStairs2(n: number): number {
+  const dp: number[] = [0, 1, 2]
+  for (let i = 3; i <= n; i++)
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+  return dp[n]
+}
+
+// scroll array
+export function climbStairs(n: number): number {
+  if (n < 3)
+    return n
+
+  let p = 0
+  let q = 1
+  let t = 2
+
+  for (let i = 3; i <= n; i++) {
+    p = q + t
+    q = t
+    t = p
+  }
+
+  return p
+}
--- packages/code/test/algorithms/dynamic-programming/070.test.ts
+++ packages/code/test/algorithms/dynamic-programming/070.test.ts
@@ -3,6 +3,10 @@ import { climbStairs } from '../../../src/algorithms/dynamic-programming/070'
 
 describe('climbStairs', () => {
   it('exported', () => {
-    expect(climbStairs(2)).toBe(3)
+    expect(climbStairs(1)).toBe(1)
+    expect(climbStairs(2)).toBe(2)
+    expect(climbStairs(3)).toBe(3)
+    expect(climbStairs(4)).toBe(5)
+    expect(climbStairs(5)).toBe(8)
   })
 })
