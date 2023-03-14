745e2b0c4cc63b4fa84e62ceb32dc0a8f572db42
danielzhang183 <danielzhang182@gmail.com>
2023-03-14 09:45:49 +0800

    question: dp 122 medium

--- packages/code/src/algorithms/dynamic-programming/122.ts
+++ packages/code/src/algorithms/dynamic-programming/122.ts
@@ -1,3 +1,25 @@
-export function maxProfit(prices: number[]): number {
+// DP
+export function maxProfit1(prices: number[]): number {
+  const dp: number[][] = [[-prices[0], 0]]
+  const len = prices.length
+  for (let i = 1; i < len; i++) {
+    dp[i] = []
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i])
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+  }
 
-};
+  return dp[len - 1][1]
+}
+
+// Greedy
+export function maxProfit(prices: number[]): number {
+  let profit = 0
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1])
+      profit += prices[i] - prices[i - 1]
+  }
+
+  return profit
+}
+
+maxProfit([7, 1, 5, 3, 6, 4])
--- packages/code/test/algorithms/dynamic-programming/122.test.ts
+++ packages/code/test/algorithms/dynamic-programming/122.test.ts
@@ -3,6 +3,8 @@ import { maxProfit } from '../../../src/algorithms/dynamic-programming/122'
 
 describe('maxProfit', () => {
   it('exported', () => {
-    expect(maxProfit([7,1,5,3,6,4])).toBe([1,2,3,4,5])
+    expect(maxProfit([7, 1, 5, 3, 6, 4])).toBe(7)
+    expect(maxProfit([1, 2, 3, 4, 5])).toBe(4)
+    expect(maxProfit([7, 6, 4, 3, 1])).toBe(0)
   })
 })
