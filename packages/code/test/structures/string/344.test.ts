c0771491f0689c948ab83ee4f7184d166bec8cbf
danielzhang183 <danielzhang182@gmail.com>
2023-03-14 17:45:19 +0800

    question: string 344 easy

--- packages/code/test/structures/string/344.test.ts
+++ packages/code/test/structures/string/344.test.ts
@@ -3,6 +3,7 @@ import { reverseString } from '../../../src/structures/string/344'
 
 describe('reverseString', () => {
   it('exported', () => {
-    expect(reverseString(['h', 'e', 'l', 'l', 'o'])).toBe(['H', 'a', 'n', 'n', 'a', 'h'])
+    expect(reverseString(['h', 'e', 'l', 'l', 'o'])).toStrictEqual(['o', 'l', 'l', 'e', 'h'])
+    expect(reverseString(['H', 'a', 'n', 'n', 'a', 'h'])).toStrictEqual(['h', 'a', 'n', 'n', 'a', 'H'])
   })
 })
