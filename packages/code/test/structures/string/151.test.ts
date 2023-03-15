461e98ebd7a109a5d5e0b89750b4e52185ffd696
danielzhang183 <danielzhang182@gmail.com>
2023-03-16 00:36:58 +0800

    question: string 151 medium

--- packages/code/test/structures/string/151.test.ts
+++ packages/code/test/structures/string/151.test.ts
@@ -3,6 +3,8 @@ import { reverseWords } from '../../../src/structures/string/151'
 
 describe('reverseWords', () => {
   it('exported', () => {
-    expect(reverseWords('the sky is blue')).toBe('  hello world  ')
+    expect(reverseWords('the sky is blue')).toBe('blue is sky the')
+    expect(reverseWords('  hello world  ')).toBe('world hello')
+    expect(reverseWords('a good   example')).toBe('example good a')
   })
 })
