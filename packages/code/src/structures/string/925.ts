0afea1856225c390bfb79c66bf66718154cf04fc
danielzhang183 <danielzhang182@gmail.com>
2023-03-16 15:49:38 +0800

    question: string 925 easy

--- packages/code/src/structures/string/925.ts
+++ packages/code/src/structures/string/925.ts
@@ -1,3 +1,44 @@
-export function isLongPressedName(name: string, typed: string): boolean {
+// Two Pointers
+export function isLongPressedName1(name: string, typed: string): boolean {
+  let i = name.length - 1
+  let j = typed.length - 1
+  let repeat = name[i]
+  while (1) {
+    if (name[i] !== typed[j]) {
+      while (typed[j] === repeat)
+        j--
+    }
+    if (i < 0 || j < 0)
+      break
+    if (name[i] !== typed[j])
+      return false
+    repeat = typed[j]
+    i--
+    j--
+  }
 
+  return i === -1 && j === -1
 }
+
+export function isLongPressedName(name: string, typed: string): boolean {
+  let i = 0
+  let j = 0
+  while (i < name.length && j < typed.length) {
+    if (name[i] === typed[j]) {
+      i++
+      j++
+    }
+    else if (i > 0 && typed[j] === name[i - 1]) {
+      j++
+    }
+    else {
+      break
+    }
+  }
+
+  return i === name.length && j === typed.length
+}
+
+// isLongPressedName('alex', 'aaleex')
+// isLongPressedName('saeed', 'ssaaeddd')
+isLongPressedName('alex', 'aaleexa')
--- packages/code/test/structures/string/925.test.ts
+++ packages/code/test/structures/string/925.test.ts
@@ -3,7 +3,8 @@ import { isLongPressedName } from '../../../src/structures/string/925'
 
 describe('isLongPressedName', () => {
   it('exported', () => {
-    expect(isLongPressedName('alex')).toBe('aaleex')
-    expect(isLongPressedName('saeed')).toBe('ssaaedd')
+    expect(isLongPressedName('alex', 'aaleex')).toBe(true)
+    expect(isLongPressedName('saeed', 'ssaaedd')).toBe(false)
+    expect(isLongPressedName('alex', 'aaleexa')).toBe(false)
   })
 })
