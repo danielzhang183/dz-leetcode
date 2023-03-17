095b7a1093c0c5ea1faee3bdee2bf969d7354e68
danielzhang183 <danielzhang182@gmail.com>
2023-03-17 13:38:57 +0800

    question: hashtable 001 easy

--- packages/code/src/structures/hash-table/001.ts
+++ packages/code/src/structures/hash-table/001.ts
@@ -1,3 +1,11 @@
 export function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>()
+  for (let i = 0; i < nums.length; i++) {
+    const rest = target - nums[i]
+    if (map.has(rest))
+      return [map.get(rest)!, i]
+    map.set(nums[i], i)
+  }
 
+  return []
 }
