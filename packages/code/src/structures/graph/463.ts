1196b3fd5629b817170b6b2e116897ea7e87a757
danielzhang183 <danielzhang182@gmail.com>
2023-03-18 12:02:17 +0800

    question: graph 463 easy

--- packages/code/src/structures/graph/463.ts
+++ packages/code/src/structures/graph/463.ts
@@ -1,3 +1,44 @@
-export function islandPerimeter(grid: number[][]): number {
+export function islandPerimeter1(grid: number[][]): number {
+  let land = 0
+  let cover = 0
+  const m = grid.length
+  const n = grid[0].length
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] !== 1)
+        continue
+      land++
+      if (i - 1 >= 0 && grid[i - 1][j] === 1)
+        cover++
+      if (j - 1 >= 0 && grid[i][j - 1] === 1)
+        cover++
+    }
+  }
 
+  return land * 4 - cover * 2
 }
+
+export function islandPerimeter(grid: number[][]): number {
+  let count = 0
+  const m = grid.length
+  const n = grid[0].length
+  const directX = [-1, 1, 0, 0]
+  const directY = [0, 0, -1, 1]
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] !== 1)
+        continue
+      for (let k = 0; k < 4; k++) {
+        const x = i + directX[k]
+        const y = j + directY[k]
+        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] === 0) {
+          count++
+          continue
+        }
+      }
+    }
+  }
+
+  return count
+}
+
--- packages/code/test/structures/graph/463.test.ts
+++ packages/code/test/structures/graph/463.test.ts
@@ -3,6 +3,8 @@ import { islandPerimeter } from '../../../src/structures/graph/463'
 
 describe('islandPerimeter', () => {
   it('exported', () => {
-    expect(islandPerimeter([[0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]])).toBe([[1]])
+    expect(islandPerimeter([[0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]])).toBe(16)
+    expect(islandPerimeter([[1]])).toBe(4)
+    expect(islandPerimeter([[1, 0]])).toBe(4)
   })
 })
