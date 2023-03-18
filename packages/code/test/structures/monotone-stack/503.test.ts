4b54dc7349b7a930efaea4870c269daa667a74b9
danielzhang183 <danielzhang182@gmail.com>
2023-03-18 18:43:16 +0800

    question: montone-stack 503 easy

--- packages/code/test/structures/monotone-stack/503.test.ts
+++ packages/code/test/structures/monotone-stack/503.test.ts
@@ -3,6 +3,7 @@ import { nextGreaterElements } from '../../../src/structures/monotone-stack/503'
 
 describe('nextGreaterElements', () => {
   it('exported', () => {
-    expect(nextGreaterElements([1, 2, 1])).toBe([1, 2, 3, 4, 3])
+    expect(nextGreaterElements([1, 2, 1])).toStrictEqual([2, -1, 2])
+    expect(nextGreaterElements([1, 2, 3, 4, 3])).toStrictEqual([2, 3, 4, -1, 4])
   })
 })
