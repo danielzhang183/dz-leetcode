6810274b8528ef8fd5982a8b1d54652508121644
danielzhang183 <danielzhang182@gmail.com>
2023-03-18 12:20:14 +0800

    question: graph 841 medium

--- packages/code/src/structures/graph/841.ts
+++ packages/code/src/structures/graph/841.ts
@@ -1,3 +1,32 @@
+// DFS
 export function canVisitAllRooms(rooms: number[][]): boolean {
+  const seen: boolean[] = new Array(rooms.length).fill(false)
+  const dfs = (room: number) => {
+    if (seen[room])
+      return
+    seen[room] = true
+    for (const key of rooms[room])
+      dfs(key)
+  }
 
+  dfs(0)
+  return seen.every(Boolean)
+}
+
+// BFS
+export function canVisitAllRooms1(rooms: number[][]): boolean {
+  const stack: number[][] = [rooms[0]]
+  const seen: boolean[] = new Array(rooms.length).fill(false)
+  seen[0] = true
+
+  while (stack.length) {
+    for (const key of stack.pop()!) {
+      if (!seen[key]) {
+        seen[key] = true
+        stack.push(rooms[key])
+      }
+    }
+  }
+
+  return seen.every(Boolean)
 }
