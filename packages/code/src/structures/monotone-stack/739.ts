59ee533f1d4997ff5663bb170d017266302545e8
danielzhang183 <danielzhang182@gmail.com>
2023-03-18 13:37:02 +0800

    question: montone-stack 739 medium

--- packages/code/src/structures/monotone-stack/739.ts
+++ packages/code/src/structures/monotone-stack/739.ts
@@ -1,3 +1,17 @@
 export function dailyTemperatures(temperatures: number[]): number[] {
+  const n = temperatures.length
+  const ans: number[] = new Array(n).fill(0)
+  const stack: number[] = []
+  for (let i = 0; i < n; i++) {
+    while (
+      stack.length > 0
+      && temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      const top = stack.pop()!
+      ans[top] = i - top
+    }
+    stack.push(i)
+  }
 
+  return ans
 }
--- packages/code/test/structures/monotone-stack/739.test.ts
+++ packages/code/test/structures/monotone-stack/739.test.ts
@@ -3,6 +3,8 @@ import { dailyTemperatures } from '../../../src/structures/monotone-stack/739'
 
 describe('dailyTemperatures', () => {
   it('exported', () => {
-    expect(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])).toBe([30, 40, 50, 60])
+    expect(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])).toStrictEqual([1, 1, 4, 2, 1, 1, 0, 0])
+    expect(dailyTemperatures([30, 40, 50, 60])).toStrictEqual([1, 1, 1, 0])
+    expect(dailyTemperatures([30, 60, 90])).toStrictEqual([1, 1, 0])
   })
 })
