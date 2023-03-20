dfb84ce857510115d5499d981a54fcc1e784c32a
danielzhang183 <danielzhang182@gmail.com>
2023-03-20 18:35:59 +0800

    question: ll 024 medium

--- packages/code/src/structures/linked-list/024.ts
+++ packages/code/src/structures/linked-list/024.ts
@@ -1,15 +1,20 @@
-export /**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+import { ListNode } from '../../utils/types'
 
-function swapPairs(head: ListNode | null): ListNode | null {
+export function swapPairs(head: ListNode | null): ListNode | null {
+  if (head == null || head.next == null)
+    return head
 
+  const dump = new ListNode(-1, head)
+
+  let cur: ListNode | null = dump
+  while (cur?.next != null && cur.next.next != null) {
+    const tmp = cur.next
+    const tmp1 = cur.next.next.next
+    cur.next = cur.next.next
+    cur.next.next = tmp
+    cur.next.next!.next = tmp1
+    cur = cur.next.next
+  }
+
+  return dump.next
 }
--- packages/code/test/structures/linked-list/024.test.ts
+++ packages/code/test/structures/linked-list/024.test.ts
@@ -1,8 +1,11 @@
 import { describe, expect, it } from 'vitest'
 import { swapPairs } from '../../../src/structures/linked-list/024'
+import { createLinkedList, toLinkedListString } from '../../../src/utils/linkedList'
 
 describe('swapPairs', () => {
   it('exported', () => {
-    expect(swapPairs([1, 2, 3, 4])).toBe([])
+    expect(toLinkedListString(swapPairs(createLinkedList([1, 2, 3, 4])))).toStrictEqual([2, 1, 4, 3])
+    expect(toLinkedListString(swapPairs(createLinkedList([1])))).toStrictEqual([1])
+    expect(toLinkedListString(swapPairs(createLinkedList([])))).toStrictEqual([])
   })
 })
