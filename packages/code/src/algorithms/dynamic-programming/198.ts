b0ea536cbb5764c7e1fcbb38122525bdc718765d
danielzhang183 <danielzhang182@gmail.com>
2023-03-21 22:53:22 +0800

    question: dp 198 medium

--- packages/code/src/algorithms/dynamic-programming/198.ts
+++ packages/code/src/algorithms/dynamic-programming/198.ts
@@ -1,3 +1,31 @@
-export function rob(nums: number[]): number {
+// DP
+export function rob1(nums: number[]): number {
+  const n = nums.length
+  if (n === 1)
+    return nums[0]
+  if (n === 2)
+    return Math.max(nums[0], nums[1])
+  const dp: number[] = [nums[0], Math.max(nums[0], nums[1])]
+  for (let i = 2; i < n; i++)
+    dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2])
 
+  return dp[n - 1]
+}
+
+// scroll array
+export function rob(nums: number[]): number {
+  const n = nums.length
+  if (n === 1)
+    return nums[0]
+  if (n === 2)
+    return Math.max(nums[0], nums[1])
+  let first = nums[0]
+  let second = Math.max(nums[0], nums[1])
+  for (let i = 2; i < n; i++) {
+    const tmp = second
+    second = Math.max(first + nums[i], second)
+    first = tmp
+  }
+
+  return second
 }
--- packages/code/test/algorithms/dynamic-programming/198.test.ts
+++ packages/code/test/algorithms/dynamic-programming/198.test.ts
@@ -3,6 +3,7 @@ import { rob } from '../../../src/algorithms/dynamic-programming/198'
 
 describe('rob', () => {
   it('exported', () => {
-    expect(rob([1, 2, 3, 1])).toBe([2, 7, 9, 3, 1])
+    expect(rob([1, 2, 3, 1])).toBe(4)
+    expect(rob([2, 7, 9, 3, 1])).toBe(12)
   })
 })
