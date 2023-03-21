84e3a393632cc8ecc19083ba16aa31aa68b5682c
danielzhang183 <danielzhang182@gmail.com>
2023-03-21 22:53:51 +0800

    question: dp 213 medium

--- packages/code/src/algorithms/dynamic-programming/213.ts
+++ packages/code/src/algorithms/dynamic-programming/213.ts
@@ -1,3 +1,44 @@
-export function rob(nums: number[]): number {
+// DP
+export function rob1(nums: number[]): number {
+  const n = nums.length
+  if (n === 1)
+    return nums[0]
+  if (n === 2)
+    return Math.max(nums[0], nums[1])
 
+  const helper = (start: number, end: number) => {
+    const dp: number[] = []
+    dp[start] = nums[start]
+    dp[start + 1] = Math.max(nums[start], nums[start + 1])
+    for (let i = start + 2; i < end; i++)
+      dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2])
+    return dp[dp.length - 1]
+  }
+
+  return Math.max(helper(0, n - 1), helper(1, n))
 }
+
+// scroll array
+export function rob(nums: number[]): number {
+  const n = nums.length
+  if (n === 1)
+    return nums[0]
+  if (n === 2)
+    return Math.max(nums[0], nums[1])
+
+  const helper = (start: number, end: number) => {
+    let first = nums[start]
+    let second = Math.max(nums[start], nums[start + 1])
+    for (let i = start + 2; i < end; i++) {
+      const tmp = second
+      second = Math.max(second, first + nums[i])
+      first = tmp
+    }
+
+    return second
+  }
+
+  return Math.max(helper(0, n - 1), helper(1, n))
+}
+
+rob([1, 2, 3, 1])
--- packages/code/test/algorithms/dynamic-programming/213.test.ts
+++ packages/code/test/algorithms/dynamic-programming/213.test.ts
@@ -3,6 +3,8 @@ import { rob } from '../../../src/algorithms/dynamic-programming/213'
 
 describe('rob', () => {
   it('exported', () => {
-    expect(rob([2, 3, 2])).toBe([1, 2, 3, 1])
+    expect(rob([2, 3, 2])).toBe(3)
+    expect(rob([1, 2, 3, 1])).toBe(4)
+    expect(rob([1, 2, 3])).toBe(3)
   })
 })
