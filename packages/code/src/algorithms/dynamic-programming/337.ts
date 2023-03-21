e4212eb543d5d7d73486fafb486fa67aa373c6dd
danielzhang183 <danielzhang182@gmail.com>
2023-03-22 07:00:46 +0800

    question: dp 337 medium

--- packages/code/src/algorithms/dynamic-programming/337.ts
+++ packages/code/src/algorithms/dynamic-programming/337.ts
@@ -1,17 +1,18 @@
-export /**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import type { TreeNode } from './../../utils/tree'
 
-function rob(root: TreeNode | null): number {
+// DFS
+export function rob(root: TreeNode | null): number {
+  const dfs = (root: TreeNode | null) => {
+    if (root == null)
+      return [0, 0]
 
+    const l = dfs(root.left)
+    const r = dfs(root.right)
+    const selected = root.val! + l![1] + r![1]
+    const noSelected = Math.max(l![0], l![1]) + Math.max(r![0], r![1])
+    return [selected, noSelected]
+  }
+
+  const ans = dfs(root)
+  return Math.max(ans[0], ans[1])
 }
--- packages/code/test/algorithms/dynamic-programming/337.test.ts
+++ packages/code/test/algorithms/dynamic-programming/337.test.ts
@@ -1,8 +1,10 @@
 import { describe, expect, it } from 'vitest'
 import { rob } from '../../../src/algorithms/dynamic-programming/337'
+import { createBinaryTree } from '../../../src/utils/tree'
 
 describe('rob', () => {
   it('exported', () => {
-    expect(rob([3, 2, 3, null, 3, null, 1])).toBe([3, 4, 5, 1, 3, null, 1])
+    expect(rob(createBinaryTree([3, 2, 3, null, 3, null, 1]))).toBe(7)
+    expect(rob(createBinaryTree([3, 4, 5, 1, 3, null, 1]))).toBe(9)
   })
 })
