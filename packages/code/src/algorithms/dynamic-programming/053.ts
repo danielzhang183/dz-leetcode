8b4edb981336c0227a92ffd72c0cfeda06c02ad4
danielzhang183 <danielzhang182@gmail.com>
2023-03-22 09:31:17 +0800

    question: dp 053 medium

--- packages/code/src/algorithms/dynamic-programming/053.ts
+++ packages/code/src/algorithms/dynamic-programming/053.ts
@@ -1,3 +1,33 @@
-export function maxSubArray(nums: number[]): number {
+// DP
+export function maxSubArray1(nums: number[]): number {
+  const n = nums.length
+  if (n === 1)
+    return nums[0]
 
+  let max = nums[0]
+  const dp: number[] = [0, nums[0]]
+  for (let i = 2; i <= n; i++) {
+    dp[i] = Math.max(nums[i - 1] + dp[i - 1], nums[i - 1])
+    max = Math.max(max, dp[i])
+  }
+
+  return max
 }
+
+// greedy
+export function maxSubArray(nums: number[]): number {
+  const n = nums.length
+  if (n === 1)
+    return nums[0]
+
+  let max = nums[0]
+  let sum = nums[0]
+  for (let i = 2; i <= n; i++) {
+    sum = Math.max(nums[i - 1] + sum, nums[i - 1])
+    max = Math.max(max, sum)
+  }
+
+  return max
+}
+
+maxSubArray([5, 4, -1, 7, 8])
--- packages/code/test/algorithms/dynamic-programming/053.test.ts
+++ packages/code/test/algorithms/dynamic-programming/053.test.ts
@@ -3,6 +3,8 @@ import { maxSubArray } from '../../../src/algorithms/dynamic-programming/053'
 
 describe('maxSubArray', () => {
   it('exported', () => {
-    expect(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe([1])
+    expect(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe(6)
+    expect(maxSubArray([1])).toBe(1)
+    expect(maxSubArray([5, 4, -1, 7, 8])).toBe(23)
   })
 })
