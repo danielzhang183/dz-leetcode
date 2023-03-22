d2c84984499091fc7a8f43298adb983a4e0bf875
danielzhang183 <danielzhang182@gmail.com>
2023-03-22 20:26:37 +0800

    question: dp 1143 medium

--- packages/code/src/algorithms/dynamic-programming/1143.ts
+++ packages/code/src/algorithms/dynamic-programming/1143.ts
@@ -1,3 +1,17 @@
 export function longestCommonSubsequence(text1: string, text2: string): number {
+  const [m, n] = [text1.length, text2.length]
+  const dp: number[][] = Array.from(
+    { length: m + 1 },
+    () => Array.from({ length: n + 1 }, () => 0),
+  )
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (text1[i - 1] === text2[j - 1])
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      else
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+    }
+  }
 
+  return dp[m][n]
 }
--- packages/code/test/algorithms/dynamic-programming/1143.test.ts
+++ packages/code/test/algorithms/dynamic-programming/1143.test.ts
@@ -2,9 +2,9 @@ import { describe, expect, it } from 'vitest'
 import { longestCommonSubsequence } from '../../../src/algorithms/dynamic-programming/1143'
 
 describe('longestCommonSubsequence', () => {
-  it.skip('exported', () => {
-    expect(longestCommonSubsequence('abcde')).toBe('ace')
-    expect(longestCommonSubsequence('abc')).toBe('abc')
-    expect(longestCommonSubsequence('abc')).toBe('def')
+  it('exported', () => {
+    expect(longestCommonSubsequence('abcde', 'ace')).toBe(3)
+    expect(longestCommonSubsequence('abc', 'abc')).toBe(3)
+    expect(longestCommonSubsequence('abc', 'def')).toBe(0)
   })
 })
