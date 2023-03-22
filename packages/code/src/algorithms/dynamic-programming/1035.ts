766f118fae0ca037510559d978639df1d24e72b0
danielzhang183 <danielzhang182@gmail.com>
2023-03-22 20:58:54 +0800

    question: dp 1035 medium

--- packages/code/src/algorithms/dynamic-programming/1035.ts
+++ packages/code/src/algorithms/dynamic-programming/1035.ts
@@ -1,3 +1,17 @@
 export function maxUncrossedLines(nums1: number[], nums2: number[]): number {
+  const [m, n] = [nums1.length, nums2.length]
+  const dp: number[][] = Array.from(
+    { length: m + 1 },
+    () => new Array(n + 1).fill(0),
+  )
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (nums1[i - 1] === nums2[j - 1])
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      else
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+    }
+  }
 
+  return dp[m][n]
 }
