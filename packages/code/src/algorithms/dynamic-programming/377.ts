efa92c1acc60172f1ab874ab6cfa4ecec54ef2c1
danielzhang183 <danielzhang182@gmail.com>
2023-03-26 14:41:42 +0800

    question: dp 377 medium

--- packages/code/src/algorithms/dynamic-programming/377.ts
+++ packages/code/src/algorithms/dynamic-programming/377.ts
@@ -1,3 +1,16 @@
 export function combinationSum4(nums: number[], target: number): number {
+  const dp: number[] = new Array(target + 1).fill(0)
+  dp[0] = 1
 
+  const len = nums.length
+  for (let i = 1; i <= target; i++) {
+    for (let j = 0; j < len; j++) {
+      if (i >= nums[j])
+        dp[i] += dp[i - nums[j]]
+    }
+  }
+
+  return dp[target]
 }
+
+combinationSum4([1, 2, 3], 4)
--- packages/code/test/algorithms/dynamic-programming/377.test.ts
+++ packages/code/test/algorithms/dynamic-programming/377.test.ts
@@ -3,7 +3,7 @@ import { combinationSum4 } from '../../../src/algorithms/dynamic-programming/377
 
 describe('combinationSum4', () => {
   it('exported', () => {
-    expect(combinationSum4([1, 2, 3])).toBe(4)
-    expect(combinationSum4([9])).toBe(3)
+    expect(combinationSum4([1, 2, 3], 4)).toBe(4)
+    expect(combinationSum4([9], 3)).toBe(3)
   })
 })
