4a5ca16ac0b6e87cf042af9896ca75e38ec1a0c6
danielzhang183 <danielzhang182@gmail.com>
2023-03-27 17:08:26 +0800

    question: dp 322 medium

--- packages/code/src/algorithms/dynamic-programming/322.ts
+++ packages/code/src/algorithms/dynamic-programming/322.ts
@@ -1,3 +1,27 @@
 export function coinChange(coins: number[], amount: number): number {
+  const dp: number[] = new Array(amount + 1).fill(amount + 1)
+  dp[0] = 0
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (i >= coins[j])
+        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1)
+    }
+  }
 
+  return dp[amount] > amount ? -1 : dp[amount]
 }
+
+export function coinChange1(coins: number[], amount: number): number {
+  const dp: number[] = new Array(amount + 1).fill(Infinity)
+  dp[0] = 0
+  for (const coin of coins) {
+    for (let j = coin; j <= amount; j++) {
+      if (dp[j - coin] !== Infinity)
+        dp[j] = Math.min(dp[j], dp[j - coin] + 1)
+    }
+  }
+
+  return dp[amount] === Infinity ? -1 : dp[amount]
+}
+
+coinChange([1, 2, 5], 11)
--- packages/code/test/algorithms/dynamic-programming/322.test.ts
+++ packages/code/test/algorithms/dynamic-programming/322.test.ts
@@ -3,8 +3,8 @@ import { coinChange } from '../../../src/algorithms/dynamic-programming/322'
 
 describe('coinChange', () => {
   it('exported', () => {
-    expect(coinChange([1, 2, 5])).toBe(11)
-    expect(coinChange([2])).toBe(3)
-    expect(coinChange([1])).toBe(0)
+    expect(coinChange([1, 2, 5], 11)).toBe(3)
+    expect(coinChange([2], 3)).toBe(-1)
+    expect(coinChange([1], 0)).toBe(0)
   })
 })
