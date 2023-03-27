b6245e94fdfa101a13eb53c537dc2868004ae5bd
danielzhang183 <danielzhang182@gmail.com>
2023-03-28 07:18:37 +0800

    question: sort 912 medium

--- packages/code/src/algorithms/sort/912.ts
+++ packages/code/src/algorithms/sort/912.ts
@@ -1,3 +1,35 @@
+// Quick Sort
 export function sortArray(nums: number[]): number[] {
+  const swap = (arr: number[], i: number, j: number) => {
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+  }
 
+  const partition = (l: number, r: number) => {
+    // const randomIndex = Math.floor(Math.random() * (r - l + 1)) + l
+    // swap(nums, randomIndex, r)
+
+    let i = l
+    let j = r
+    while (i < j) {
+      while (i < j && nums[j] >= nums[l]) j--
+      while (i < j && nums[i] <= nums[l]) i++
+      if (i !== j)
+        swap(nums, i, j)
+    }
+    swap(nums, i, l)
+    return i + 1
+  }
+
+  const quickSort = (l: number, r: number) => {
+    if (l < r) {
+      const pos = partition(l, r)
+      quickSort(l, pos - 1)
+      quickSort(pos, r)
+    }
+  }
+
+  quickSort(0, nums.length - 1)
+  return nums
 }
--- packages/code/test/algorithms/sort/912.test.ts
+++ packages/code/test/algorithms/sort/912.test.ts
@@ -3,6 +3,7 @@ import { sortArray } from '../../../src/algorithms/sort/912'
 
 describe('sortArray', () => {
   it('exported', () => {
-    expect(sortArray([5, 2, 3, 1])).toBe([5, 1, 1, 2, 0, 0])
+    expect(sortArray([5, 2, 3, 1])).toStrictEqual([1, 2, 3, 5])
+    // expect(sortArray([5, 1, 1, 2, 0, 0])).toStrictEqual([0, 0, 1, 1, 2, 5])
   })
 })
