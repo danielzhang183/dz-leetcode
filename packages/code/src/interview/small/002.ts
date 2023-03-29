5fb18a998fd7c3854ddbdadcc49a436c548e1c3e
danielzhang183 <danielzhang182@gmail.com>
2023-03-29 14:12:51 +0800

    question: interview 002 medium

--- packages/code/src/interview/small/002.ts
+++ packages/code/src/interview/small/002.ts
@@ -1,15 +1,23 @@
-export /**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+import { ListNode } from '../../utils/types'
 
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  let addOne = 0
+  const dump = new ListNode(-1)
+  let curr: ListNode | null = dump
 
-};
+  while (addOne || l1 || l2) {
+    const val1 = l1 != null ? l1.val : 0
+    const val2 = l2 != null ? l2.val : 0
+    const sum = val1 + val2 + addOne
+    addOne = sum >= 10 ? 1 : 0
+    curr.next = new ListNode(sum % 10)
+    curr = curr!.next
+
+    if (l1)
+      l1 = l1.next
+    if (l2)
+      l2 = l2.next
+  }
+
+  return dump.next
+}
--- packages/code/test/interview/small/002.test.ts
+++ packages/code/test/interview/small/002.test.ts
@@ -1,10 +1,25 @@
 import { describe, expect, it } from 'vitest'
 import { addTwoNumbers } from '../../../src/interview/small/002'
+import { createLinkedList, toLinkedListString } from '../../../src/utils/linkedList'
 
 describe('addTwoNumbers', () => {
   it('exported', () => {
-    expect(addTwoNumbers([2,4,3])).toBe([5,6,4])
-    expect(addTwoNumbers([0])).toBe([0])
-    expect(addTwoNumbers([9,9,9,9,9,9,9])).toBe([9,9,9,9])
+    expect(toLinkedListString(addTwoNumbers(
+      createLinkedList([2, 4, 3]),
+      createLinkedList([5, 6, 4]),
+    )))
+      .toStrictEqual([7, 0, 8])
+
+    expect(toLinkedListString(addTwoNumbers(
+      createLinkedList([0]),
+      createLinkedList([0]),
+    )))
+      .toStrictEqual([0])
+
+    expect(toLinkedListString(addTwoNumbers(
+      createLinkedList([9, 9, 9, 9, 9, 9, 9]),
+      createLinkedList([9, 9, 9, 9]),
+    )))
+      .toStrictEqual([8, 9, 9, 9, 0, 0, 0, 1])
   })
 })
