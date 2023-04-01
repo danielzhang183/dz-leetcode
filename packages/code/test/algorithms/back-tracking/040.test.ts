9c3db1de5e895ba0de629e7b0e2e962217efd7bb
danielzhang183 <danielzhang182@gmail.com>
2023-04-01 16:50:02 +0800

    question: bs 040 medium

--- packages/code/test/algorithms/back-tracking/040.test.ts
+++ packages/code/test/algorithms/back-tracking/040.test.ts
@@ -3,7 +3,7 @@ import { combinationSum2 } from '../../../src/algorithms/back-tracking/040'
 
 describe('combinationSum2', () => {
   it('exported', () => {
-    expect(combinationSum2([10, 1, 2, 7, 6, 1, 5])).toBe(8)
-    expect(combinationSum2([2, 5, 2, 1, 2])).toBe(5)
+    expect(combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)).toStrictEqual([[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]])
+    expect(combinationSum2([2, 5, 2, 1, 2], 5)).toStrictEqual([[1, 2, 2], [5]])
   })
 })
