a764dd69f294c2d4096e6c5c9d2f66c736cd7609
danielzhang183 <danielzhang182@gmail.com>
2023-04-02 17:23:29 +0800

    question: bt 079 hard

--- packages/code/src/algorithms/back-tracking/079.ts
+++ packages/code/src/algorithms/back-tracking/079.ts
@@ -1,3 +1,70 @@
-export function exist(board: string[][], word: string): boolean {
+export function exist1(board: string[][], word: string): boolean {
+  const row = board.length
+  const col = board[0].length
+  const visited = Array.from({ length: row }, () => Array(col).fill(false))
+  const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+  let ans = false
 
+  const dfs = (i: number, j: number, k: number) => {
+    if (board[i][j] !== word[k])
+      return false
+    else if (k === word.length - 1)
+      return true
+
+    visited[i][j] = true
+    for (const [dx, dy] of directions) {
+      const [x, y] = [i + dx, j + dy]
+      if (x >= 0 && x < row && y >= 0 && y < col) {
+        if (!visited[x][y]) {
+          ans = dfs(x, y, k + 1)
+          if (ans)
+            break
+        }
+      }
+    }
+    visited[i][j] = false
+    return ans
+  }
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      ans = dfs(i, j, 0)
+      if (ans)
+        return true
+    }
+  }
+
+  return false
+}
+
+export function exist(board: string[][], word: string): boolean {
+  const row = board.length
+  const col = board[0].length
+  const len = word.length
+  let ans = false
+  const dfs = (i: number, j: number, k: number) => {
+    if ((board[i] || [])[j] !== word[k])
+      return false
+    else if (k === len - 1)
+      return true
+
+    board[i][j] = ''
+    ans = dfs(i - 1, j, k + 1)
+      || dfs(i + 1, j, k + 1)
+      || dfs(i, j - 1, k + 1)
+      || dfs(i, j + 1, k + 1)
+    if (!ans)
+      board[i][j] = word[k]
+    return ans
+  }
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      ans = dfs(i, j, 0)
+      if (ans)
+        return true
+    }
+  }
+
+  return false
 }
--- packages/code/test/algorithms/back-tracking/079.test.ts
+++ packages/code/test/algorithms/back-tracking/079.test.ts
@@ -2,9 +2,12 @@ import { describe, expect, it } from 'vitest'
 import { exist } from '../../../src/algorithms/back-tracking/079'
 
 describe('exist', () => {
-  it('exported', () => {
-    expect(exist([['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']])).toBe('ABCCED')
-    expect(exist([['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']])).toBe('SEE')
-    expect(exist([['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']])).toBe('ABCB')
-  })
+  const createMatrix = () => [
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E'],
+  ]
+  it('ABCCED', () => expect(exist(createMatrix(), 'ABCCED')).toBe(true))
+  it('SEE', () => expect(exist(createMatrix(), 'SEE')).toBe(true))
+  it('ABCB', () => expect(exist(createMatrix(), 'ABCB')).toBe(false))
 })
