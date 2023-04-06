2ada812dc19fcaa83ff7f1d313a590760843a06c
danielzhang183 <danielzhang182@gmail.com>
2023-04-06 23:52:26 +0800

    question: hash-table 454 medium

--- packages/code/src/structures/hash-table/454.ts
+++ packages/code/src/structures/hash-table/454.ts
@@ -1,3 +1,14 @@
+// partition + map
 export function fourSumCount(nums1: number[], nums2: number[], nums3: number[], nums4: number[]): number {
+  const map = new Map()
+  nums1.forEach(i => nums2.forEach(j => map.set(i + j, (map.get(i + j) || 0) + 1)))
+  let ans = 0
+  for (let i of nums3) {
+    for (const j of nums4) {
+      if (map.has(-i - j))
+        ans += map.get(-i - j)
+    }
+  }
 
+  return ans
 }
--- packages/code/test/structures/hash-table/454.test.ts
+++ packages/code/test/structures/hash-table/454.test.ts
@@ -3,9 +3,7 @@ import { fourSumCount } from '../../../src/structures/hash-table/454'
 
 describe('fourSumCount', () => {
   it('exported', () => {
-    expect(fourSumCount([1, 2])).toBe([-2, -1])
-    expect(fourSumCount([-1, 2])).toBe([0, 2])
-    expect(fourSumCount([0])).toBe([0])
-    expect(fourSumCount([0])).toBe([0])
+    expect(fourSumCount([1, 2],[-2, -1], [-1, 2], [0, 2])).toBe(2)
+    expect(fourSumCount([0], [0], [0], [0])).toBe(1)
   })
 })
