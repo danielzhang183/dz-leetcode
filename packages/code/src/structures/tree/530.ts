86da1f1760d4f080889bc48175775e3b3f6c0f4b
danielzhang183 <danielzhang182@gmail.com>
2023-04-09 13:34:34 +0800

    question: tree 530 easy

--- packages/code/src/structures/tree/530.ts
+++ packages/code/src/structures/tree/530.ts
@@ -1,17 +1,57 @@
-export /**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+import type { TreeNode } from '../../utils'
 
-function getMinimumDifference(root: TreeNode | null): number {
+// DFS
+export function getMinimumDifference1(root: TreeNode | null): number {
+  let min = Infinity
+  let prev = -1
+  const dfs = (root: TreeNode | null) => {
+    if (root == null)
+      return
 
+    dfs(root.left)
+    if (prev !== -1)
+      min = Math.min(min, root.val! - prev)
+    prev = root.val!
+    dfs(root.right)
+  }
+
+  dfs(root)
+  return min
+}
+
+// Morris
+
+export function getMinimumDifference(root: TreeNode | null): number {
+  let min = Infinity
+  let prev = -1
+  const update = (val: number) => {
+    if (prev !== -1)
+      min = Math.min(min, val - prev)
+    prev = val
+  }
+
+  let predecessor: TreeNode | null = null
+  while (root) {
+    if (root.left) {
+      predecessor = root.left
+      while (predecessor.right != null && predecessor.right !== root)
+        predecessor = predecessor.right
+
+      if (predecessor!.right == null) {
+        predecessor!.right = root
+        root = root.left
+      }
+      else {
+        update(root.val!)
+        predecessor!.right = null
+        root = root.right
+      }
+    }
+    else {
+      update(root.val!)
+      root = root.right
+    }
+  }
+
+  return min
 }
