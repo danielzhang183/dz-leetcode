ae46eca23d3588f34645c1328a5344d85212e7da
danielzhang183 <danielzhang182@gmail.com>
2023-04-09 21:39:27 +0800

    question: ll 146 medium

--- packages/code/test/structures/linked-list/146.test.ts
+++ packages/code/test/structures/linked-list/146.test.ts
@@ -1,8 +1,39 @@
 import { describe, expect, it } from 'vitest'
-import { fn } from '../../../src/structures/linked-list/146'
+import { LRUCache } from '../../../src/structures/linked-list/146'
 
-describe('fn', () => {
+describe('LRUCache', () => {
   it('exported', () => {
-    expect(fn(['LRUCache', 'put', 'put', 'get', 'put', 'get', 'put', 'get', 'get', 'get'])).toBe([[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]])
+    const lRUCache = new LRUCache(2)
+    lRUCache.put(1, 1) // cache is {1=1}
+    expect(lRUCache.entries).toBe('{1=1}')
+    lRUCache.put(2, 2) // cache is {1=1, 2=2}
+    expect(lRUCache.entries).toBe('{2=2, 1=1}')
+    expect(lRUCache.get(1)).toBe(1) // return 1
+    expect(lRUCache.entries).toBe('{1=1, 2=2}')
+    lRUCache.put(3, 3) // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    expect(lRUCache.entries).toBe('{3=3, 1=1}')
+    expect(lRUCache.get(2)).toBe(-1) // returns -1 (not found)
+    lRUCache.put(4, 4) // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    expect(lRUCache.entries).toBe('{4=4, 3=3}')
+    expect(lRUCache.get(1)).toBe(-1) // return -1 (not found)
+    expect(lRUCache.get(3)).toBe(3) // return 3
+    expect(lRUCache.get(4)).toBe(4) // return 4
+  })
+
+  it('exported1', () => {
+    const lRUCache = new LRUCache(2)
+    lRUCache.put(1, 0)
+    expect(lRUCache.entries).toBe('{1=0}')
+    lRUCache.put(2, 2)
+    expect(lRUCache.entries).toBe('{2=2, 1=0}')
+    expect(lRUCache.get(1)).toBe(0)
+    lRUCache.put(3, 3)
+    expect(lRUCache.entries).toBe('{3=3, 1=0}')
+    expect(lRUCache.get(2)).toBe(-1)
+    lRUCache.put(4, 4)
+    expect(lRUCache.entries).toBe('{4=4, 3=3}')
+    expect(lRUCache.get(1)).toBe(-1)
+    expect(lRUCache.get(3)).toBe(3)
+    expect(lRUCache.get(4)).toBe(4)
   })
 })
