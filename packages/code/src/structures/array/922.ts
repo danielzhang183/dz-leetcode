17abfdbd52a289edfdc36bd253fd6052ae4405a4
danielzhang183 <danielzhang182@gmail.com>
2023-04-10 14:03:44 +0800

    question: tree 922 easy

--- packages/code/src/structures/array/922.ts
+++ packages/code/src/structures/array/922.ts
@@ -1,3 +1,18 @@
+// Two Pointers
 export function sortArrayByParityII(nums: number[]): number[] {
+  const n = nums.length
+  let j = 1
 
+  for (let i = 0; i < n; i += 2) {
+    if (nums[i] & 1) {
+      while (nums[j] & 1)
+        j += 2
+      ;[nums[i], nums[j]] = [nums[j], nums[i]]
+    }
+  }
+
+  return nums
 }
+
+sortArrayByParityII([4, 2, 5, 7])
+// [4,5,2,7] [4,7,2,5], [2,5,4,7], [2,7,4,5]
--- packages/code/test/structures/array/922.test.ts
+++ packages/code/test/structures/array/922.test.ts
@@ -2,7 +2,15 @@ import { describe, expect, it } from 'vitest'
 import { sortArrayByParityII } from '../../../src/structures/array/922'
 
 describe('sortArrayByParityII', () => {
+  const check = (arr: number[]) => {
+    return arr.every((item, index) => index & 1 ? item & 1 : item % 2 === 0)
+  }
+
   it('exported', () => {
-    expect(sortArrayByParityII([4, 2, 5, 7])).toBe([2, 3])
+    const sorted = sortArrayByParityII([4, 2, 5, 7])
+    expect(sorted).toHaveLength(4)
+    expect(sorted).toSatisfy(check) // [4,5,2,7] [4,7,2,5], [2,5,4,7], [2,7,4,5] can be accepted
+    expect(sortArrayByParityII([2, 3])).toStrictEqual([2, 3])
   })
 })
+2
