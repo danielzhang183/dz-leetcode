df1fa9dd76c2205a68a1ee5c036b5667ef3432a4
danielzhang183 <danielzhang182@gmail.com>
2023-04-10 18:07:52 +0800

    question: array 189 medium

--- packages/code/src/structures/array/189.ts
+++ packages/code/src/structures/array/189.ts
@@ -1,6 +1,50 @@
-export /**
+/**
  Do not return anything, modify nums in-place instead.
  */
-function rotate(nums: number[], k: number): void {
+export function rotate2(nums: number[], k: number): void {
+  const n = nums.length
+  const arr = new Array(n)
+  for (let i = 0; i < n; i++)
+    arr[(i + k) % n] = nums[i]
 
+  for (let i = 0; i < n; i++)
+    nums[i] = arr[i]
 }
+
+// Reverse
+export function rotate1(nums: number[], k: number): void {
+  const reverse = (i: number, j: number) => {
+    while (i < j) {
+      [nums[i], nums[j]] = [nums[j], nums[i]]
+      i++
+      j--
+    }
+  }
+
+  reverse(0, nums.length - 1)
+  reverse(0, k - 1)
+  reverse(k, nums.length - 1)
+}
+
+// cycle replacement
+export function rotate(nums: number[], k: number): void {
+  const gcd = (x, y) => y ? gcd(y, x % y) : x
+  const n = nums.length
+  k = k % n
+  const count = gcd(k, n)
+  for (let start = 0; start < count; ++start) {
+    let current = start
+    let prev = nums[start]
+    do {
+      const next = (current + k) % n
+      const temp = nums[next]
+      nums[next] = prev
+      prev = temp
+      current = next
+    } while (start !== current)
+  }
+}
+
+// const arr = [1, 2, 3, 4, 5, 6, 7]
+// rotate(arr, 3) // [5, 6, 7, 1, 2, 3, 4]
+// console.log(arr)
--- packages/code/test/structures/array/189.test.ts
+++ packages/code/test/structures/array/189.test.ts
@@ -2,8 +2,15 @@ import { describe, expect, it } from 'vitest'
 import { rotate } from '../../../src/structures/array/189'
 
 describe('rotate', () => {
-  it('exported', () => {
-    expect(rotate([1, 2, 3, 4, 5, 6, 7])).toBe(3)
-    expect(rotate([-1, -100, 3, 99])).toBe(2)
+  it('[1, 2, 3, 4, 5, 6, 7]', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7]
+    rotate(arr, 3)
+    expect(arr).toStrictEqual([5, 6, 7, 1, 2, 3, 4])
+  })
+
+  it('[-1, -100, 3, 99]', () => {
+    const arr = [-1, -100, 3, 99]
+    rotate(arr, 2)
+    expect(arr).toStrictEqual([3, 99, -1, -100])
   })
 })
