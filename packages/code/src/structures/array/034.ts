122d4858298386ad70d490ec110f4f8674b144ce
danielzhang183 <danielzhang182@gmail.com>
2023-04-10 13:04:51 +0800

    question: array 034 easy

--- packages/code/src/structures/array/034.ts
+++ packages/code/src/structures/array/034.ts
@@ -1,3 +1,25 @@
 export function searchRange(nums: number[], target: number): number[] {
+  const n = nums.length
+  let l = 0
+  let r = n - 1
+  while (l < r) {
+    const mid = (l + r) >> 1
+    if (nums[mid] > target) {
+      r--
+    }
+    else if (nums[mid] < target) {
+      l++
+    }
+    else {
+      let i = mid
+      let j = mid
+      while (i >= 0 && nums[i] === nums[--i]);
+      while (j <= n && nums[j] === nums[++j]);
+      return [i + 1, j - 1]
+    }
+  }
 
+  return nums[l] === target ? [l, l] : [-1, -1]
 }
+
+// searchRange([5, 7, 7, 8, 8, 10], 8)
--- packages/code/test/structures/array/034.test.ts
+++ packages/code/test/structures/array/034.test.ts
@@ -3,8 +3,10 @@ import { searchRange } from '../../../src/structures/array/034'
 
 describe('searchRange', () => {
   it('exported', () => {
-    expect(searchRange([5, 7, 7, 8, 8, 10])).toBe(8)
-    expect(searchRange([5, 7, 7, 8, 8, 10])).toBe(6)
-    expect(searchRange([])).toBe(0)
+    expect(searchRange([5, 7, 7, 8, 8, 10], 8)).toStrictEqual([3, 4])
+    expect(searchRange([5, 7, 7, 8, 8, 10], 6)).toStrictEqual([-1, -1])
+    expect(searchRange([], 0)).toStrictEqual([-1, -1])
+    expect(searchRange([1], 1)).toStrictEqual([0, 0])
+    expect(searchRange([2, 2], 2)).toStrictEqual([0, 1])
   })
 })
