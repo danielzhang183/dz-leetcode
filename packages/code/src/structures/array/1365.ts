3e14b54f969cc0ffbbbc1c7eabd29a5e290c584a
danielzhang183 <danielzhang182@gmail.com>
2023-04-10 15:21:34 +0800

    question: array 1365 easy

--- packages/code/src/structures/array/1365.ts
+++ packages/code/src/structures/array/1365.ts
@@ -1,3 +1,35 @@
-export function smallerNumbersThanCurrent(nums: number[]): number[] {
+// Sort `O(N * logN) O(N)`
+export function smallerNumbersThanCurrent1(nums: number[]): number[] {
+  const n = nums.length
+  const arr: number[][] = nums.map((n, i) => [n, i])
+  arr.sort((a, b) => a[0] - b[0])
 
+  const ans: number[] = new Array(n).fill(0)
+  let prev = -1
+  for (let i = 0; i < n; i++) {
+    if (prev === -1 || arr[i][0] !== arr[i - 1][0])
+      prev = i
+    ans[arr[i][1]] = prev
+  }
+
+  return ans
 }
+
+// Count Sort O(N + K) O(K)
+export function smallerNumbersThanCurrent(nums: number[]): number[] {
+  const n = nums.length
+  const arr = new Array(101).fill(0)
+  for (let i = 0; i < n; i++)
+    arr[nums[i]] += 1
+
+  for (let i = 1; i <= 100; i++)
+    arr[i] += arr[i - 1]
+
+  const ans: number[] = []
+  for (let i = 0; i < n; i++)
+    ans.push(nums[i] ? arr[nums[i] - 1] : 0)
+
+  return ans
+}
+
+smallerNumbersThanCurrent([8, 1, 2, 2, 3]) // [4, 0, 1, 1, 3]
--- packages/code/test/structures/array/1365.test.ts
+++ packages/code/test/structures/array/1365.test.ts
@@ -3,6 +3,8 @@ import { smallerNumbersThanCurrent } from '../../../src/structures/array/1365'
 
 describe('smallerNumbersThanCurrent', () => {
   it('exported', () => {
-    expect(smallerNumbersThanCurrent([8, 1, 2, 2, 3])).toBe([6, 5, 4, 8])
+    expect(smallerNumbersThanCurrent([8, 1, 2, 2, 3])).toStrictEqual([4, 0, 1, 1, 3])
+    expect(smallerNumbersThanCurrent([6, 5, 4, 8])).toStrictEqual([2, 1, 0, 3])
+    expect(smallerNumbersThanCurrent([7, 7, 7, 7])).toStrictEqual([0, 0, 0, 0])
   })
 })
